2b670ef9b1ba696509ee7a71e7df4539305a4279
chevalier3as <chevalier3as@gmail.com>
2021-11-17 15:20:59 +0100

    Adding sample agent.

---
+++ tests/agent/sampel_agent/Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.8
+
+RUN pip3 install ostorlab
+
+
+RUN ostorlab agent run --name sampleagent --file ostorlab.yaml
---
+++ tests/agent/sampel_agent/sample.py
@@ -0,0 +1,24 @@
+from ostorlab import Agent, Message, ReportMixin
+
+
+class SampleAgent(Agent, ReportMixin):
+
+    def __init__(self, speed, performance):
+        self._speed = speed
+
+
+    def process(self, message: Message) -> None:
+        print('hello agent')
+        self.emit(
+            'a.b.c',
+            message
+        )
+
+        self.report_vulnerability(
+
+        )
+
+
+class Sample2Agent(Agent):
+    def process(self, message: Message) -> None:
+        print('hello')
