d92422981d9b4a288e49a66e903aee6240795503
chevalier3as <chevalier3as@gmail.com>
2021-12-21 22:16:06 +0100

    Adding MQ service for use by the local runtime.

---
+++ src/ostorlab/runtimes/local/services/mq.py
@@ -0,0 +1,100 @@
+import binascii
+import logging
+import os
+
+import docker
+import docker.models.services
+from docker import errors
+import docker.types
+import tenacity
+
+logger = logging.getLogger(__name__)
+
+MQ_IMAGE = 'rabbitmq:3.9-management'
+
+
+class LocalRabbitMQ:
+    def __init__(self,
+                 name: str,
+                 network: str,
+                 image: str = MQ_IMAGE) -> None:
+
+        self._name = name
+        self._docker_client = docker.from_env()
+        # images
+        self._mq_image = image
+        self._network = network
+        self._mq_host = f'mq_{self._name}'
+        # service
+        self._mq_service = None
+
+    @property
+    def url(self):
+        return f'amqp://guest:guest@{self._mq_host}:5672/'
+
+    @property
+    def vhost(self):
+        return '/'
+
+    def start(self) -> None:
+        """Start local rabbit mq instance."""
+        self._create_network()
+        self._mq_service = self._start_mq()
+
+        if not self._is_service_healthy():
+            logger.error('MQ container for service %s is not ready', self._mq_service.id)
+            return
+
+    def stop(self):
+        for service in self._docker_client.services.list():
+            universe = service.attrs['Spec']['Labels']['ostorlab.universe']
+            if service.name.startswith('mq_') and self._name in universe:
+                service.remove()
+
+    def _create_network(self):
+        if any(network.name == self._network for network in self._docker_client.networks.list()):
+            logger.warning('network already exists.')
+        else:
+            logger.info('creating private network %s', self._network)
+            return self._docker_client.networks.create(
+                name=self._network,
+                driver='overlay',
+                attachable=True,
+                labels={'ostorlab.universe': self._name, },
+                check_duplicate=True
+                )
+
+    def _start_mq(self) -> docker.models.services.Service:
+        logger.info('starting MQ')
+        endpoint_spec = docker.types.services.EndpointSpec(mode='vip')
+        service_mode = docker.types.services.ServiceMode('replicated', replicas=1)
+        return self._docker_client.services.create(
+            image=self._mq_image,
+            networks=[self._network],
+            name=self._mq_host,
+            env=[
+                'TASK_ID={{.Task.Slot}}',
+                f'MQ_SERVICE_NAME={self._mq_host}',
+                f'RABBITMQ_ERLANG_COOKIE={binascii.hexlify(os.urandom(10)).decode()}',
+            ],
+            restart_policy=docker.types.RestartPolicy(condition='any'),
+            mode=service_mode,
+            labels={'ostorlab.universe': self._name, 'ostorlab.mq': ''},
+            endpoint_spec=endpoint_spec)
+
+    @tenacity.retry(stop=tenacity.stop_after_attempt(20),
+                    wait=tenacity.wait_exponential(multiplier=1, max=12),
+                    # return last value and don't raise RetryError exception.
+                    retry_error_callback=lambda lv: lv.result(),
+                    retry=tenacity.retry_if_result(lambda v: v is False))
+    def _is_service_healthy(self) -> bool:
+        logger.info('checking service %s', self._mq_service.name)
+        return self.is_healthy
+
+    @property
+    def is_healthy(self) -> bool:
+        try:
+            return len([task for task in self._mq_service.tasks() if task['Status']['State'] == 'running']) == 1
+        except errors.DockerException:
+            return False
+
--- src/ostorlab/runtimes/runtime.py
+++ src/ostorlab/runtimes/runtime.py
@@ -1,15 +1,37 @@
-from typing import List
-
 import abc
 import dataclasses
+from typing import List, Iterable, Optional, Dict
 
 from ostorlab.assets import Asset
 
 
 @dataclasses.dataclass
+class AgentDefinition:
+    """Data class holding attributes of an agent."""
+    name: str
+    path: str
+    container_image: str
+    args: Iterable = ()
+    constraints: List[str] = None
+    mounts: Iterable = ()
+    restart_policy: str = 'any'
+    mem_limit: int = None
+    open_ports: Optional[Dict[int, int]] = None
+    replicas: int = 1
+    pull_image: bool = False
+    bus_username: str = 'username'
+    bus_password: str = 'password'
+
+
+@dataclasses.dataclass
+class AgentGroupDefinition:
+    """Data class holding the attributes of an agent."""
+
+
+@dataclasses.dataclass
 class AgentRunDefinition:
-    agents: List
-    agent_groups: List
+    agents: List[AgentDefinition]
+    agent_groups: List[AgentGroupDefinition]
 
 
 class Runtime(abc.ABC):
@@ -20,9 +42,7 @@ class Runtime(abc.ABC):
         """Checks if the runtime is capable of running the provided agent run definition."""
         raise NotImplementedError()
 
-
     @abc.abstractmethod
     def scan(self, agent_run_definition: AgentRunDefinition, asset: Asset) -> None:
         """Triggers a scan using the provided agent run definition and asset target."""
         raise NotImplementedError()
-
