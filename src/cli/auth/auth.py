f99ff5c1ae6a4c2597615fa04c47b84495abbeaa
RabsonJ <phirirabsonj@gmail.com>
2021-12-22 11:48:33 +0100

    Refactor to use APIRunner instead of Runner

---
+++ src/cli/auth/auth.py
@@ -0,0 +1,22 @@
+import logging
+
+import click
+
+from apis import runner as apis_runner
+from cli.rootcli import auth
+
+logger = logging.getLogger(__name__)
+
+@auth.command()
+@click.option('--username', '-u', help='Ostorlab platform username.', required=True)
+@click.option('--password', '-p', help='Ostorlab platform password.', required=True)
+@click.option('--expires', '-e', help='Expiration time for token (m for minutes and d for days).')
+@click.pass_context
+def login(ctx, username, password, expires):
+    try:
+        api_runner = apis_runner.APIRunner(username=username, password=password,
+                                        expires=expires)
+        api_runner.authenticate()
+    except apis_runner.AuthenticationError:
+        logger.error(
+            'Authentication error, please check that your credentials are valid.')
--- src/cli/scan/android_apk_file.py
+++ src/cli/scan/android_apk_file.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 @click.pass_context
 def apk(ctx, plan, platform, application):
     try:
-        api_runner = apis_runner.Runner(username=ctx.obj['username'], password=ctx.obj['password'],
+        api_runner = apis_runner.APIRunner(username=ctx.obj['username'], password=ctx.obj['password'],
                                         proxy=ctx.obj['proxy'], verify=ctx.obj['tlsverify'])
         api_runner.authenticate()
 
