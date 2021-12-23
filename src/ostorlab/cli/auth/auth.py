43a9f21770a8c8fc41ecb17db654a7a736edcd4d
RabsonJ <phirirabsonj@gmail.com>
2021-12-23 11:14:28 +0100

    Chnage click option used to get token duration

--- src/ostorlab/cli/auth/auth.py
+++ src/ostorlab/cli/auth/auth.py
@@ -10,12 +10,12 @@ logger = logging.getLogger(__name__)
 @auth.command()
 @click.option('--username', '-u', help='Ostorlab platform username.', required=True)
 @click.option('--password', '-p', help='Ostorlab platform password.', required=True)
-@click.option('--expires', '-e', help='Expiration time for token (m for minutes, h for hours, and d for days).')
+@click.option('--token-duration', help='Expiration time for token (m for minutes, h for hours, and d for days).')
 @click.pass_context
-def login(ctx, username, password, expires):
+def login(ctx, username, password, token_duration):
     try:
         api_runner = apis_runner.APIRunner(username=username, password=password,
-                                        expires=expires)
+                                           token_duration=token_duration)
         api_runner.authenticate()
     except apis_runner.AuthenticationError:
         logger.error(
