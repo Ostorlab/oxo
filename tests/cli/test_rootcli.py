f1685923669d7aa237824e365510bb2cb3c46e0e
mouhcine <mnarmouche@gmail.com>
2021-12-24 18:04:54 +0100

    change Test naming

--- tests/cli/test_rootcli.py
+++ tests/cli/test_rootcli.py
@@ -3,16 +3,23 @@ from click.testing import CliRunner
 from ostorlab.cli import rootcli
 
 
-def testRootCli_WithNoOptions_ShowAvailableOptions():
+def testRootCli_whenNoOptionProvided_showAvailableOptions():
     """Test ostorlab main command 'Ostorlab' with no options and no sub command."""
+
     runner = CliRunner()
+
     result = runner.invoke(rootcli.rootcli, [''])
-    assert "Usage: rootcli [OPTIONS]" in result.output
+
+    assert 'Usage: rootcli [OPTIONS]' in result.output
     assert result.exit_code == 2
 
 
-def testRootCli_WrongCommand_ShowNoSuchCommandErrorAndExit():
+def testRootCli_whenWrongCommandIsProvided_showNoSuchCommandErrorAndExit():
+    """Run CLI with wrong command."""
+
     runner = CliRunner()
+
     result = runner.invoke(rootcli.rootcli, ['wrong-command'])
+
     assert "No such command 'wrong-command'" in result.output
     assert result.exit_code == 2
