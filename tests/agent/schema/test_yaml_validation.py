86b166d8a2225bd551d77d0fa58fb827177d5aae
deadly-panda <abderrahim.haddadiii@gmail.com>
2021-12-26 18:02:44 +0100

    Fixed the tests.

--- tests/agent/schema/test_yaml_validation.py
+++ tests/agent/schema/test_yaml_validation.py
@@ -1,21 +1,15 @@
-"""Tests for the validation of Yaml configuration files against json schema files.
-"""
+"""Tests for the validation of Yaml configuration files against json schema files."""
 
 import io
 
 import pytest
 
-from src.ostorlab.agent.schema.validator import Validator
-from src.ostorlab.agent.schema.validator import ValidationError, SchemaError
+from ostorlab.agent.schema import validator
 
 
 def testYamlValidation_whenItIsCorrect_noRaise(json_schema_file):
     """Unit test for the method that validates a yaml configuration file against a json schema.
     Case where the yaml configuration file is valid.
-
-    Args:
-      json_schema_file:
-        py test fixture for the json schema file object.
     """
 
     valid_yaml_data = """
@@ -37,26 +31,21 @@ def testYamlValidation_whenItIsCorrect_noRaise(json_schema_file):
     """
 
     with io.StringIO(valid_yaml_data) as yaml_data_file:
-        validator = Validator(json_schema_file)
+        validator_object = validator.Validator(json_schema_file)
 
         try:
-            validator.validate(yaml_data_file)
-        except ValidationError:
+            validator_object.validate(yaml_data_file)
+        except validator.ValidationError:
             pytest.fail("ValidationError shouldn't be expected.")
-        except SchemaError:
+        except validator.SchemaError:
             pytest.fail("SchemaError shouldn't be expected.")
 
 
-def testYamlValidation_whenItIsWrong_raisesValidationError(json_schema_file):
+def testYamlValidation_whenSourceParamDoesNotRespectURLPattern_raisesValidationError(json_schema_file):
     """Unit test for the method that validates a yaml configuration file against a json schema.
-    Case where the yaml configuration file is invalid.
-
-    Args:
-      json_schema_file:
-        py test fixture for the json schema file object.
+    Case where the yaml configuration file is invalid : The parameter 'source' should be a URL instead of a number.
     """
 
-    # source value bellow should be a URL instead of the given number 3.
     invalid_yaml_data = """
         name: Agent1
         description: Agent1 Description should be here
@@ -74,18 +63,19 @@ def testYamlValidation_whenItIsWrong_raisesValidationError(json_schema_file):
         - out_selector2
         restart_policy: run_once
     """
-    validator = Validator(json_schema_file)
+    validator_object = validator.Validator(json_schema_file)
     yaml_data_file = io.StringIO(invalid_yaml_data)
 
-    with pytest.raises(ValidationError) as exc_info:
-        validator.validate(yaml_data_file)
+    with pytest.raises(validator.ValidationError) as exc_info:
+        validator_object.validate(yaml_data_file)
 
-    assert exc_info.type is ValidationError
+    assert exc_info.type is validator.ValidationError
 
 
-def testValidatorInit_whenSchemaIsInvalid_raisesSchemaError():
+def testValidatorInit_whenSchemaIsInvalidTypeParamIsMisspelled_raisesSchemaError():
     """Unit test for the init of the validator class with a invalid schema.
     """
+
     # the invalid part : type : "strg".
     invalid_json_schema = """
         {
@@ -99,7 +89,7 @@ def testValidatorInit_whenSchemaIsInvalid_raisesSchemaError():
     """
     json_schema_file_object = io.StringIO(invalid_json_schema)
 
-    with pytest.raises(SchemaError) as exc_info:
-        _ = Validator(json_schema_file_object)
+    with pytest.raises(validator.SchemaError) as exc_info:
+        _ = validator.Validator(json_schema_file_object)
 
-    assert exc_info.type is SchemaError
+    assert exc_info.type is validator.SchemaError
