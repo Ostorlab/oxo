452705b97788da2756ff9d25b2fcf61e3507e3c8
deadly-panda <abderrahim.haddadiii@gmail.com>
2021-12-26 23:13:16 +0100

    fixed  test.

--- tests/agent/schema/test_yaml_validation.py
+++ tests/agent/schema/test_yaml_validation.py
@@ -13,21 +13,21 @@ def testYamlValidation_whenItIsCorrect_noRaise(json_schema_file):
     """
 
     valid_yaml_data = """
-        name: Agent1
-        description: Agent1 Description should be here
-        image: some/path/to/the/image
-        source: https://github.com/
-        durability: development
+        name: "Agent1"
+        description: "Agent1 Description should be here"
+        image: "some/path/to/the/image"
+        source: "https://github.com/"
+        durability: "development"
         restrictions: 
-        - restriction1
-        - restriction2
+        - "restriction1"
+        - "restriction2"
         in_selectors: 
-        - in_selector1
-        - in_selector2
+        - "in_selector1"
+        - "in_selector2"
         out_selectors:
-        - out_selector1
-        - out_selector2
-        restart_policy: run_once
+        - "out_selector1"
+        - "out_selector2"
+        restart_policy: "run_once"
     """
 
     with io.StringIO(valid_yaml_data) as yaml_data_file:
@@ -47,21 +47,21 @@ def testYamlValidation_whenSourceParamDoesNotRespectURLPattern_raisesValidationE
     """
 
     invalid_yaml_data = """
-        name: Agent1
-        description: Agent1 Description should be here
-        image: some/path/to/the/image
+        name: "Agent1"
+        description: "Agent1 Description should be here"
+        image: "some/path/to/the/image"
         source: 3
-        durability: development
+        durability: "development"
         restrictions: 
-        - restriction1
-        - restriction2
+        - "restriction1"
+        - "restriction2"
         in_selectors: 
-        - in_selector1
-        - in_selector2
+        - "in_selector1"
+        - "in_selector2"
         out_selectors:
-        - out_selector1
-        - out_selector2
-        restart_policy: run_once
+        - "out_selector1"
+        - "out_selector2"
+        restart_policy: "run_once"
     """
     validator_object = validator.Validator(json_schema_file)
     yaml_data_file = io.StringIO(invalid_yaml_data)
@@ -74,9 +74,9 @@ def testYamlValidation_whenSourceParamDoesNotRespectURLPattern_raisesValidationE
 
 def testValidatorInit_whenSchemaIsInvalidTypeParamIsMisspelled_raisesSchemaError():
     """Unit test for the init of the validator class with a invalid schema.
+    The type is misspelled : 'strg' instead of 'string'.
     """
 
-    # the invalid part : type : "strg".
     invalid_json_schema = """
         {
             "properties": {
