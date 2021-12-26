24f7bd0aa473b07a71dc6fea99920e598d971ee1
mouhcine <mnarmouche@gmail.com>
2021-12-26 16:08:53 +0100

    typos

--- src/ostorlab/runtimes/__init__.py
+++ src/ostorlab/runtimes/__init__.py
@@ -1,5 +1,5 @@
 """Ostorlab runtime module.
-Runtimes are in charge of running scan as defines by a set of agents, agent group and a target asset."""
+Runtimes are in charge of running a scan as defines by a set of agents, agent group and a target asset."""
 
 from .local.runtime import LocalRuntime
 from .runtime import AgentRunDefinition, AgentGroupDefinition, Runtime, AgentDefinition
