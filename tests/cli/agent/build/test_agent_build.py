f3f615b0aa7d50f89772ac17cc5b3c228e3b007b
deadly-panda <abderrahim.haddadiii@gmail.com>
2021-12-27 14:23:06 +0100

    added cleanup fixture

--- tests/cli/agent/build/test_agent_build.py
+++ tests/cli/agent/build/test_agent_build.py
@@ -21,10 +21,15 @@ def testAgentBuildCLI_whenRequiredOptionFileIsMissing_showMessage():
     assert "Error: Missing option '--file' / '-f'." in result.output
 
 
-def testAgentBuildCLI_whenCommandIsValid_buildCompletedAndNoRaiseImageNotFoundExcep(docker_sdk_client):
+def testAgentBuildCLI_whenCommandIsValid_buildCompletedAndNoRaiseImageNotFoundExcep(docker_dummy_image_cleanup):
     """Test ostorlab agent build CLI command : Case where the command is valid.
     The agent container should be built.
     """
+    dummy_def_yaml_file_path = "./assets/dummydef.yaml"
+    runner = testing.CliRunner()
+    _ = runner.invoke(rootcli.rootcli, ["agent", "build", f"--file={dummy_def_yaml_file_path}"])
+    docker_sdk_client = docker.from_env()
+
     try:
         docker_sdk_client.images.get("dummy_agent")
     except docker.errors.ImageNotFound:
--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,6 @@ import ruamel.yaml
 from ostorlab.cli import rootcli
 
 
-
 @pytest.fixture
 def json_schema_file():
     """Json schema is made a fixture since it will be used by multiple unit tests.
@@ -102,22 +101,10 @@ def json_schema_file():
 
 
 @pytest.fixture
-def docker_sdk_client():
-    """Runs the agent build command, yields the docker sdk client & clean-up.
-    Yields:
-        docker sdk client
+def docker_dummy_image_cleanup():
+    """Pytest fixture for removing all dummy images.
     """
-    
-    dummy_def_yaml_file_path = "./assets/dummydef.yaml"
-    runner = testing.CliRunner()
-
-    _ = runner.invoke(rootcli.rootcli, ["agent", "build", f"--file={dummy_def_yaml_file_path}"])
-
-    yaml = ruamel.yaml.YAML(typ='safe')
-    with open(dummy_def_yaml_file_path, 'r') as def_file:
-        dummy_def_data = yaml.load(def_file)
-        image_name = dummy_def_data["name"]
-
     client = docker.from_env()
     yield client
-    client.images.remove(image_name)
+    for img in client.images.list():
+        _ = [client.images.remove(t) for t in img.tags if "dummy" in t]
