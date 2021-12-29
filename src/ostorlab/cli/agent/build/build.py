985a00e4a7961d2ad89a30e89aaa558e32806ce2
chevalier3as <chevalier3as@gmail.com>
2021-12-29 12:32:17 +0100

    Fix agent build returning none due to file read.

--- src/ostorlab/cli/agent/build/build.py
+++ src/ostorlab/cli/agent/build/build.py
@@ -1,5 +1,5 @@
-"""Agent Build commands."""
-
+'""Agent Build commands.""'
+import io
 import pathlib
 import logging
 
@@ -7,38 +7,38 @@ import click
 import docker
 import ruamel.yaml
 
-from ostorlab.cli import rootcli
+from ostorlab.cli.agent import agent
 from ostorlab.agent.schema import validator
 
-AGENT_SPEC_PATH = pathlib.Path(__file__).parent.parent.parent.parent / "agent/schema/agent_schema.json"
+AGENT_SPEC_PATH = pathlib.Path(__file__).parent.parent.parent.parent / 'agent/schema/agent_schema.json'
 logger = logging.getLogger(__name__)
 
 
-@rootcli.agent.command()
-@click.option("--file", "-f", help="Path to Agent yaml definition.", required=True)
-def build(file: str) -> None:
+@agent.command()
+@click.option('--file', '-f', type=click.File('rb'), help='Path to Agent yaml definition.', required=True)
+def build(file: io.FileIO) -> None:
     """CLI command to build the agent container from a definition.yaml file.
     Usage : Ostorlab agent build -f path/to/definition.yaml
     """
 
-    with open(AGENT_SPEC_PATH, "r", encoding="utf8") as agent_spec:
+    with open(AGENT_SPEC_PATH, 'r', encoding='utf8') as agent_spec:
         try:
             yaml_def_validator = validator.Validator(agent_spec)
         except validator.SchemaError:
-            logger.error("Schema is invalid.")
+            logger.error('Schema is invalid.')
 
     try:
         yaml_def_validator.validate(file)
+        file.seek(0)
+        yaml = ruamel.yaml.YAML(typ='safe')
+        agent_def = yaml.load(file)
+
+        dockerfile_path = agent_def['docker_file_path']
+        docker_build_root = agent_def['docker_build_root']
+        container_name = agent_def['name']
+
+        docker_sdk_client = docker.from_env()
+        docker_sdk_client.images.build(path=docker_build_root, dockerfile=dockerfile_path, tag=container_name)
     except validator.ValidationError:
-        logger.error("Definition file does not conform to the provided specification.")
+        logger.error('Definition file does not conform to the provided specification.')
 
-    yaml = ruamel.yaml.YAML(typ="safe")
-    with open(file, "r", encoding="utf8") as def_file:
-        agent_def = yaml.load(def_file)
-
-    dockerfile_path = agent_def["docker_file_path"]
-    docker_build_root = agent_def["docker_build_root"]
-    container_name = agent_def["name"]
-
-    docker_sdk_client = docker.from_env()
-    docker_sdk_client.images.build(path=docker_build_root, dockerfile=dockerfile_path, tag= container_name)
