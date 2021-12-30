dd9bf5dfc67a0fadc3eb382010df3a5bef32da23
deadly-panda <abderrahim.haddadiii@gmail.com>
2021-12-30 09:59:43 +0100

    removed utils.schema

--- src/ostorlab/utils/schemas.py
+++
@@ -1,6 +0,0 @@
-"""Constants of the paths for the Agent & Agent Group specifications."""
-import pathlib
-
-OSTORLAB_ROOT_PATH = pathlib.Path().resolve().parent
-AGENT_SPEC_PATH = OSTORLAB_ROOT_PATH / "agent/schema/agent_schema.json"
-AGENT_GROUP_SPEC_PATH = OSTORLAB_ROOT_PATH / "agent/schema/agentGroup_schema.json"
