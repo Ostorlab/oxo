8790dd436dff5d872a81f4834bf0de240d94a422
deadly-panda <abderrahim.haddadiii@gmail.com>
2021-12-30 10:02:58 +0100

    Fixed linting & typo

--- src/ostorlab/runtimes/local/services/mq.py
+++ src/ostorlab/runtimes/local/services/mq.py
@@ -21,7 +21,7 @@ class LocalRabbitMQ:
     def __init__(self,
                  name: str,
                  network: str,
-                 exposed_ports: "dict[int, int]" = None,
+                 exposed_ports: 'Dict[int, int]' = None,
                  image: str = MQ_IMAGE) -> None:
         """Initialize the MQ service parameters.
         Args:
