8c73bd9e39004c9a6420e237f081294db2190bb2
RabsonJ <phirirabsonj@gmail.com>
2021-12-31 09:47:04 +0100

    Refactor: store ostorlab private diractory in a constant

--- src/ostorlab/configuration_manager.py
+++ src/ostorlab/configuration_manager.py
@@ -7,6 +7,8 @@
 
 import pathlib
 
+OSTORLAB_PRIVATE_DIR = pathlib.Path.home() / '.ostorlab'
+
 
 class ConfigurationManager:
     """Handles any configurations related to Ostorlab, such as storing and retriving
@@ -14,26 +16,24 @@ class ConfigurationManager:
     """
 
     def __init__(self):
-        """Constructs all the necessary attributes for the object.
-        """
-        self._ostorlab_private_dir = pathlib.Path.home() / '.ostorlab'
-        self._ostorlab_private_dir.mkdir(parents=True, exist_ok=True)
-        self._complete_api_key_path = self._ostorlab_private_dir / 'key'
+        """Constructs all the necessary attributes for the object."""
+        OSTORLAB_PRIVATE_DIR.mkdir(parents=True, exist_ok=True)
+        self._complete_api_key_path = OSTORLAB_PRIVATE_DIR / 'key'
 
     def set_api_key(self, api_key: str) -> None:
-        """Persists the API key to a file in the given path
+        """Persists the API key to a file in the given path.
 
         Args:
-            api_key: The authenticated user's generated API key
+            api_key: The authenticated user's generated API key.
         """
         with open(self._complete_api_key_path, 'w', encoding='utf-8') as file:
             file.write(api_key)
 
     def get_api_key(self) -> str:
-        """Gets the API key from the location in which it is saved
+        """Gets the API key from the location in which it is saved.
 
         Returns:
-            str: The user's API key
+            str: The user's API key.
         """
         with open(self._complete_api_key_path, 'r', encoding='utf-8') as file:
             return file.read()
