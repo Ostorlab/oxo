7823aee2a94a923510c103d10e474f7abeb74df8
chevalier3as <chevalier3as@gmail.com>
2021-12-31 15:12:56 +0100

    Update worker.

--- docs/docs/service-worker.js
+++ docs/docs/service-worker.js
@@ -1,53 +1,41 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-// service-worker.js
 
-// workbox.core.setCacheNameDetails({ prefix: 'd4' })
-// //Change this value every time before you build
-// const LATEST_VERSION = 'v0.0'
-// self.addEventListener('activate', (event) => {
-//   console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
-//   if (caches) {
-//     caches.keys().then((arr) => {
-//       arr.forEach((key) => {
-//         if (key.indexOf('d4-precache') < -1) {
-//           caches.delete(key).then(() => console.log(`%c Cleared ${key}`, 'background: #333; color: #ff0000'))
-//         } else {
-//           caches.open(key).then((cache) => {
-//             cache.match('version').then((res) => {
-//               if (!res) {
-//                 cache.put('version', new Response(LATEST_VERSION, { status: 200, statusText: LATEST_VERSION }))
-//               } else if (res.statusText !== LATEST_VERSION) {
-//                 caches.delete(key).then(() => console.log(`%c Cleared Cache ${LATEST_VERSION}`, 'background: #333; color: #ff0000'))
-//               } else console.log(`%c Great you have the latest version ${LATEST_VERSION}`, 'background: #333; color: #00ff00')
-//             })
-//           })
-//         }
-//       })
-//     })
-//   }
-// })
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
-self.addEventListener('activate', function (event) {
-      event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-          console.info('deleting cache')
-          return Promise.all(
-            cacheNames
-              .filter(function (cacheName) {
-                return true
-              })
-              .map(function (cacheName) {
-                return caches.delete(cacheName)
-              }),
-          )
-        }),
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
       )
-    })
-
-console.info('From worker')
-// workbox.skipWaiting()
-// workbox.clientsClaim()
-//
-// self.__precacheManifest = [].concat(self.__precacheManifest || [])
-// workbox.precaching.suppressWarnings()
-// workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+    )
+  }
+})
