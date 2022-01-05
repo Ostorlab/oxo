441719c02afbc58ad6538fd210bb0417e2d6106f
asasas333_3 <am.mesbahi@gmail.com>
2022-01-05 12:51:47 +0100

    Minor changes

--- src/ostorlab/agent/testing/mock_agent.py
+++ src/ostorlab/agent/testing/mock_agent.py
@@ -3,24 +3,26 @@ import pytest
 
 from typing import Any, List, Dict
 
-from ostorlab.agent import message
+from ostorlab.agent import message as msg
 
 
 @pytest.fixture(scope='function')
 def agent_mock(mocker):
     """This fixtures patches all the Agent components and returns the list of messages emitted """
-    emit_message_with_selector_queue: List[message.Message] = []
+    emitted_messages: List[msg.Message] = []
     mocker.patch('ostorlab.agent.mixins.agent_mq_mixin.AgentMQMixin.mq_init', return_value=None)
     mocker.patch('ostorlab.agent.mixins.agent_mq_mixin.AgentMQMixin.mq_run', return_value=None)
     mocker.patch('ostorlab.agent.mixins.agent_mq_mixin.AgentMQMixin.mq_close', return_value=None)
     mocker.patch('ostorlab.agent.agent.AgentMixin._is_mq_healthy', return_value=True)
 
-    def emit_message(selector: str, data: Dict[str, Any]):
-        agent_message = message.Message.from_data(selector, data)
-        emit_message_with_selector_queue.append(agent_message)
+    def mq_send_message(key, message, message_priority=None):
+        # we need to remove the last part of the key f'{selector}.{uuid.uuid1()}'
+        agent_message = msg.Message.from_raw('.'.join(key.split('.')[:-1]), message)
+        emitted_messages.append(agent_message)
 
-    mocker.patch('ostorlab.agent.agent.AgentMixin.emit', side_effect=emit_message)
-    yield emit_message_with_selector_queue
+    mocker.patch('ostorlab.agent.mixins.agent_mq_mixin.AgentMQMixin.mq_send_message', side_effect=mq_send_message)
+    yield emitted_messages
+    emitted_messages = []
 
 
 
