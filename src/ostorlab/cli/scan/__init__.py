bcbc91c02e38ca95bb3daf2ec3563e8d28047fd5
RabsonJ <phirirabsonj@gmail.com>
2022-01-05 15:55:04 +0100

    fix pylint error

--- src/ostorlab/cli/scan/__init__.py
+++ src/ostorlab/cli/scan/__init__.py
@@ -1,4 +1,5 @@
 """Module for the root command: scan."""
 from ostorlab.cli.scan.scan import scan
 from ostorlab.cli.scan.run import run
+# pylint: disable=redefined-builtin
 from ostorlab.cli.scan.list import list
