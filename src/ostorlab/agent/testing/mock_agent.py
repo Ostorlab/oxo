35e824b529e073c0dbb53118919b8a2a17a75d6a
asasas333_3 <am.mesbahi@gmail.com>
2022-01-05 12:57:24 +0100

    Minor changes

--- src/ostorlab/agent/testing/mock_agent.py
+++ src/ostorlab/agent/testing/mock_agent.py
@@ -1,7 +1,7 @@
 """mock agent implements the required methods to test the agent's behaviors without using external components."""
 import pytest
 
-from typing import Any, List, Dict
+from typing import List
 
 from ostorlab.agent import message as msg
 
@@ -15,7 +15,7 @@ def agent_mock(mocker):
     mocker.patch('ostorlab.agent.mixins.agent_mq_mixin.AgentMQMixin.mq_close', return_value=None)
     mocker.patch('ostorlab.agent.agent.AgentMixin._is_mq_healthy', return_value=True)
 
-    def mq_send_message(key, message, message_priority=None):
+    def mq_send_message(key, message):
         # we need to remove the last part of the key f'{selector}.{uuid.uuid1()}'
         agent_message = msg.Message.from_raw('.'.join(key.split('.')[:-1]), message)
         emitted_messages.append(agent_message)
