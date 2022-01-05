73fc51d88964e06a2360599edab39e7d9c3dfcc5
RabsonJ <phirirabsonj@gmail.com>
2022-01-05 15:02:08 +0100

    Add tests

--- tests/cli/scan/list/test_list_scan.py
+++ tests/cli/scan/list/test_list_scan.py
@@ -2,12 +2,13 @@
 from click.testing import CliRunner
 from ostorlab.cli import rootcli
 from ostorlab.cli import console
+from ostorlab.apis import request as api_request
 
 from unittest import mock
 
 
 def testOstorlabScanListCLI_whenNoOptionsProvided_showsUsage():
-    """Test ostorlab scan command with no options and no sub command.
+    """Test ostorlab scan list command with no options and no sub command.
     Should show usage.
     """
 
@@ -18,49 +19,39 @@ def testOstorlabScanListCLI_whenNoOptionsProvided_showsUsage():
     assert 'Usage: rootcli scan list [OPTIONS]\n' in result.output
 
 
-scans_data = {
-    "data": {
-        "scans": {
-            "pageInfo": {
-                "hasNext": True,
-                "hasPrevious": False,
-                "count": 2579,
-                "numPages": 2579
-            },
-            "scans": [
-                {
-                    "assetType": "ios_store",
-                    "riskRating": None,
-                    "version": None,
-                    "packageName": None,
-                    "id": "5040",
-                    "progress": "not_started",
-                    "createdTime": "2022-01-04T12:00:24.548386+00:00",
-                    "plan": "FREE",
-                    "asset": {
-                        "__typename": "IOSStoreAssetType",
-                        "applicationName": "Drop a Message",
-                        "packageName": "com.adrianeio.dropamessage"
-                    }
-                }
-            ]
-        }
-    }
-}
-
-def testOstorlabAuthRevokeCLI_whenInvalidApiKeyIdIsProvided_logsError(
-        mock_rich_console, requests_mock):
-    """Test ostorlab auth revoke command with wrong api key id.
-    Should log the error.
+def testOstorlabScanListCLI_whenCorrectCommandsAndOptionsProvided_showsScanInfo(requests_mock):
+    """Test ostorlab scan list command with correct commands and options.
+    Should show scans information.
     """
 
-    errors_dict = {'errors': [{'message': 'OrganizationAPIKey matching query does not exist.', 'locations': [
-        {'line': 3, 'column': 16}], 'path': ['revokeApiKey']}], 'data': {'revokeApiKey': None}}
+    scans_data = {
+        'data': {
+            'scans': {
+                'pageInfo': {},
+                'scans': []
+            }
+        }
+    }
+
+    runner = CliRunner()
+    requests_mock.post(api_request.AUTHENTICATED_GRAPHQL_ENDPOINT,
+                       json=scans_data, status_code=200)
+    result = runner.invoke(rootcli.rootcli, ['scan', 'list', '--source=remote'])
+
+    assert result.exception is None
+
+@mock.patch.object(console, 'error')
+def testOstorlabScanListCLI_whenUserIsNotAuthenticated_logsError(
+        mock_rich_console, requests_mock):
+    """Test ostorlab scan list command with correct commands and options
+    but the user is not authenticated.
+    Should log an error.
+    """
 
     mock_rich_console.return_value = None
     runner = CliRunner()
     requests_mock.post(api_request.AUTHENTICATED_GRAPHQL_ENDPOINT,
-                       json=errors_dict, status_code=200)
-    result = runner.invoke(rootcli.rootcli, ['auth', 'revoke'])
+                       json=None, status_code=401)
+    result = runner.invoke(rootcli.rootcli, ['scan', 'list', '--source=remote'])
     assert result.exception is None
-    mock_rich_console.assert_called_once_with('Could not revoke your API key.')
+    mock_rich_console.assert_called()
