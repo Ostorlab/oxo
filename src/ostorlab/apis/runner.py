e8295dff826e8b27cb74af6dedb4cc54e5ffc303
RabsonJ <phirirabsonj@gmail.com>
2022-01-06 14:26:40 +0100

    Merge console branch

--- src/ostorlab/apis/runner.py
+++ src/ostorlab/apis/runner.py
@@ -20,11 +20,11 @@ from ostorlab import configuration_manager as config_manager
 from ostorlab.apis import create_api_key
 from ostorlab.apis import login
 from ostorlab.apis import request as api_request
-import rich
+from ostorlab.cli import console
 
-console = rich.console.Console()
 
 logger = logging.getLogger(__name__)
+console = console.Console()
 
 CONSOLE_LOADING_TEXT_STYLES = 'bold blue'
 CONSOLE_LOADED_TEXT_STYLES = 'bold green'
@@ -93,7 +93,7 @@ class APIRunner:
         Raises:
             AuthenticationError: If user credentials are not valid.
         """
-        with console.status(f'[{CONSOLE_LOADING_TEXT_STYLES}]Logging into your account'):
+        with console.status('Logging into your account'):
             response = self._login_user()
 
         if response.status_code != 200:
@@ -107,10 +107,10 @@ class APIRunner:
                 raise AuthenticationError(response.status_code)
         else:
             self._token = response.json().get('token')
-            with console.status(f'[{CONSOLE_LOADING_TEXT_STYLES}]Generating API key'):
+            with console.status('Generating API key'):
                 api_key_response = self.execute(
                     create_api_key.CreateAPIKeyAPIRequest(self._token_duration))
-                console.print(f'[{CONSOLE_LOADED_TEXT_STYLES}]API key generated')
+                console.success('API key generated')
 
             api_data = api_key_response['data']['createApiKey']['apiKey']
             secret_key = api_data['secretKey']
@@ -118,12 +118,12 @@ class APIRunner:
             expiry_date = api_data['apiKey']['expiryDate']
 
             self._api_key = secret_key
-            with console.status(f'[{CONSOLE_LOADING_TEXT_STYLES}]Persisting API key'):
+            with console.status('Persisting API key'):
                 self._configuration_manager.set_api_data(
                     secret_key, api_key_id, expiry_date)
-                console.print(f'[{CONSOLE_LOADED_TEXT_STYLES}]API key persisted')
+                console.success('API key persisted')
             self._token = None
-            console.print(f'[{CONSOLE_LOADED_TEXT_STYLES}]✅ Authentication successful')
+            console.success('✅ Authentication successful')
 
     def unauthenticate(self) -> None:
         self._api_key = None
@@ -146,8 +146,7 @@ class APIRunner:
             headers = {'X-Api-Key': f'{self._api_key}'}
         else:
             headers = None
-            console.print(
-                f'[{CONSOLE_WARNING_TEXT_STYLES}]No authentication credentials were provided.')
+            console.warning('No authentication credentials were provided.')
 
         response = self._sent_request(request, headers)
         if response.status_code != 200:
--- src/ostorlab/cli/auth/revoke/revoke.py
+++ src/ostorlab/cli/auth/revoke/revoke.py
@@ -6,10 +6,11 @@ from ostorlab.apis import runner as apis_runner
 from ostorlab.apis import revoke_api_key
 from ostorlab import configuration_manager
 from ostorlab.cli.auth import auth
-import rich
+from ostorlab.cli import console
 
 console = rich.console.Console()
 logger = logging.getLogger(__name__)
+console = console.Console()
 
 CONSOLE_LOADING_TEXT_STYLES = 'bold blue'
 CONSOLE_LOADED_TEXT_STYLES = 'bold green'
@@ -27,15 +28,14 @@ def revoke():
         api_key_id = config_manager.get_api_key_id()
         runner = apis_runner.APIRunner()
 
-        with console.status(f'[{CONSOLE_LOADING_TEXT_STYLES}]Revoking API key'):
+        with console.status('Revoking API key'):
             response = runner.execute(revoke_api_key.RevokeAPIKeyAPIRequest(api_key_id))
             if response.get('errors') is not None:
-                console.print(f'[{CONSOLE_ERROR_TEXT_STYLES}]Could not revoke your API key.')
+                console.error('Could not revoke your API key.')
 
             runner.unauthenticate()
             config_manager.delete_api_data()
-            console.print(f'[{CONSOLE_LOADED_TEXT_STYLES}]API key revoked')
+            console.success('API key revoked')
     except apis_runner.AuthenticationError:
         runner.unauthenticate()
-        console.print(
-            f'[{CONSOLE_LOADING_TEXT_STYLES}]Your API key has already been revoked.')
+        console.success('Your API key has already been revoked.')
