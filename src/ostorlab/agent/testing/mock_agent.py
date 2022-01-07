f52b71c096fdff99fe212e038e1c31ab08091a3f
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-07 09:35:29 +0100

    Added patches for the tracker tests

--- src/ostorlab/agent/testing/mock_agent.py
+++ src/ostorlab/agent/testing/mock_agent.py
@@ -15,6 +15,10 @@ def agent_mock(mocker):
     mocker.patch('ostorlab.agent.mixins.agent_mq_mixin.AgentMQMixin.mq_close', return_value=None)
     mocker.patch('ostorlab.agent.agent.AgentMixin._is_mq_healthy', return_value=True)
 
+    mocker.patch('ostorlab.agent.mixins.agent_healthcheck_mixin.AgentHealthcheckMixin.start_healthcheck', return_value=None)
+    mocker.patch('ostorlab.agent.mixins.agent_healthcheck_mixin.AgentHealthcheckMixin.add_healthcheck', return_value=None) 
+    mocker.patch('ostorlab.agent.mixins.agent_healthcheck_mixin.AgentHealthcheckMixin.__init__', return_value=None)
+
     def mq_send_message(key, message):
         # we need to remove the last part of the key f'{selector}.{uuid.uuid1()}'
         agent_message = msg.Message.from_raw('.'.join(key.split('.')[:-1]), message)
@@ -23,7 +27,3 @@ def agent_mock(mocker):
     mocker.patch('ostorlab.agent.mixins.agent_mq_mixin.AgentMQMixin.mq_send_message', side_effect=mq_send_message)
     yield emitted_messages
     emitted_messages = []
-
-
-
-
