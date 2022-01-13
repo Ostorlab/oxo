d04aeb30b7d3385b87fa6dd404f8f39c876492fe
RabsonJ <phirirabsonj@gmail.com>
2022-01-13 10:34:22 +0100

    Refactor logger info

--- src/ostorlab/runtimes/local/runtime.py
+++ src/ostorlab/runtimes/local/runtime.py
@@ -334,5 +334,5 @@ class LocalRuntime(runtime.Runtime):
                     )
                     scans.append(scan)
             except KeyError:
-                logger.error('The environment variables do not exist.')
+                logger.error('The label ostorlab.universe do not exist.')
         return scans[0:number_elements]
