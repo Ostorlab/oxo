a1ca408d833890d65dbb4e8ad9c0a8fbdea00ed0
chevalier3as <chevalier3as@gmail.com>
2022-02-04 10:08:19 +0100

    Minor tweaks to follow with matching changes.

--- src/ostorlab/cli/agent/install/install.py
+++ src/ostorlab/cli/agent/install/install.py
@@ -11,12 +11,10 @@ console = cli_console.Console()
 
 
 @agent.command()
-@click.option('--agent_key', '-key', help='Agent key.', required=True)
-@click.option('--tag', '-tag', help='Agent tag.', required=False)
-def install(agent_key: str, tag: str = '') -> None:
+@click.option('--agent', '-a', help='Agent key.', required=True)
+@click.option('--version', '-v', help='Agent version.', required=False)
+def install(agent: str, version: str = '') -> None:
     """CLI command to install an agent : Fetch the docker file location of the agent corresponding to the agent_key,
     and pull the image from the registry.
-    Usage : ostorlab agent install --agent_key=<agent_key> --tag=<tag>
     """
-
-    install_agent.install(agent_key, tag)
+    install_agent.install(agent, version)
