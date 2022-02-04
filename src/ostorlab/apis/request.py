031bd02fc0b56e7cc82130ea4c83fa4e2c8d68c8
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-02-04 08:55:08 +0100

    update public endpoint & docstring.

--- src/ostorlab/apis/request.py
+++ src/ostorlab/apis/request.py
@@ -1,28 +1,31 @@
-"""TODO(mohsine):Wrtite docstring."""
+""""""
 import abc
 import logging
 from typing import Dict, Optional
 
 AUTHENTICATED_GRAPHQL_ENDPOINT = 'https://api.ostorlab.co/apis/graphql'
-PUBLIC_GRAPHQL_ENDPOINT = 'https://api.ostorlab.co/apis/public'
+PUBLIC_GRAPHQL_ENDPOINT = 'https://api.ostorlab.co/apis/public_graphql'
 TOKEN_ENDPOINT = 'https://api.ostorlab.co/apis/token/'
 
 logger = logging.getLogger(__name__)
 
 
 class APIRequest(abc.ABC):
-    """TODO(mohsine):Wrtite docstring."""
+    """API request base class. ALL requests should inherit from this class."""
 
     @property
     def endpoint(self):
+        """Endpoint of the request."""
         return AUTHENTICATED_GRAPHQL_ENDPOINT
 
     @property
     @abc.abstractmethod
     def query(self) -> Optional[str]:
+        """Query to the GraphQL API."""
         raise NotImplementedError('Missing implementation')
 
     @property
     @abc.abstractmethod
     def data(self) -> Optional[Dict]:
+        """Body of the API request, containing the query & any additional data."""
         raise NotImplementedError('Missing implementation')
