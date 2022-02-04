f56ba23b8c86773110666698f892c003f48ebbfe
Amine <amine.mesbahi@ostorlab.dev>
2022-02-04 09:35:10 +0100

    Fixes

--- src/ostorlab/models/models.py
+++ src/ostorlab/models/models.py
@@ -4,8 +4,8 @@ import enum
 import datetime
 import logging
 
-from sqlalchemy import Column, Integer, Enum, ForeignKey, Table, String, DateTime, MetaData
-from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import Column, Integer, Enum, String, DateTime, MetaData
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
@@ -15,7 +15,7 @@ from ostorlab.cli import console as cli_console
 logger = logging.getLogger(__name__)
 console = cli_console.Console()
 
-ENGINE_URL = f'sqlite:///{config_manager.OSTORLAB_PRIVATE_DIR}/ostorlab_db.sqlite'
+ENGINE_URL = f'sqlite:///{config_manager.OSTORLAB_PRIVATE_DIR}/db.sqlite'
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
@@ -23,14 +23,14 @@ Base = declarative_base(metadata=metadata)
 
 class RiskRating(enum.Enum):
     """Enumeration of the risk rating of a scan."""
-    high = 'High'
-    medium = 'Medium'
-    low = 'Low'
-    potentially = 'Potentially'
-    hardening = 'Hardening'
-    secure = 'Secure'
-    important = 'Important'
-    info = 'Info'
+    HIGH = 'High'
+    MEDIUM = 'Medium'
+    LOW = 'Low'
+    POTENTIALLY = 'Potentially'
+    HARDENING = 'Hardening'
+    SECURE = 'Secure'
+    IMPORTANT = 'Important'
+    INFO = 'Info'
 
 
 class Database:
@@ -38,36 +38,35 @@ class Database:
 
     def __init__(self):
         """Constructs the database engine."""
-        self.db_engine = create_engine(ENGINE_URL)
+        self._db_engine = create_engine(ENGINE_URL)
         self.db_session = None
 
+    @property
     def session(self):
         """Session singleton to run queries on the db engine"""
         if self.db_session is None:
-            Session = sessionmaker(expire_on_commit=False)
-            Session.configure(bind=self.db_engine)
-            self.db_session = Session()
+            session_maker = sessionmaker(expire_on_commit=False)
+            session_maker.configure(bind=self._db_engine)
+            self.db_session = session_maker()
             return self.db_session
         else:
             return self.db_session
 
     def create_db_tables(self):
         """Create the database tables."""
-        try:
-            with self.db_engine.begin() as conn:
-                metadata.create_all(conn)
-                logger.info("Tables created")
-        except Exception as e:
-            logger.error("Error occurred during Table creation with the error {}".format(e))
+        with self._db_engine.begin() as conn:
+            metadata.create_all(conn)
+            logger.info('Tables created')
 
     def drop_db_tables(self):
         """Drop the database tables."""
-        metadata.drop_all(self.db_engine)
-        logger.info("Tables dropped")
+        metadata.drop_all(self._db_engine)
+        logger.info('Tables dropped')
+
 
 class Scan(Base):
-    """The model a scan."""
-    __tablename__ = "scan"
+    """The Scan model"""
+    __tablename__ = 'scan'
     id = Column(Integer, primary_key=True)
     title = Column(String(255))
     created_time = Column(DateTime)
@@ -84,7 +83,7 @@ class Scan(Base):
         """
         scan = Scan(title=title, created_time=datetime.datetime.now(), risk_rating='info')
         database = Database()
-        database.session().add(scan)
-        database.session().commit()
+        database.session.add(scan)
+        database.session.commit()
         return scan
 
