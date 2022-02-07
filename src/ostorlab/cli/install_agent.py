ddaeb162ce4c3b54112fb670fb4dffbedd8a2261
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-02-07 22:58:20 +0100

    Install agent.

--- src/ostorlab/cli/install_agent.py
+++ src/ostorlab/cli/install_agent.py
@@ -11,6 +11,37 @@ from ostorlab import configuration_manager
 console = cli_console.Console()
 
 
+def _parse_repository_tag(repo_name):
+    parts = repo_name.rsplit('@', 1)
+    if len(parts) == 2:
+        return tuple(parts)
+    parts = repo_name.rsplit(':', 1)
+    if len(parts) == 2 and '/' not in parts[1]:
+        return tuple(parts)
+    return repo_name, None
+
+def get_repo_name_and_tag(repository, tag=None):
+    repository, image_tag = _parse_repository_tag(repository)
+    tag = tag or image_tag or 'latest'
+    return repository, tag
+
+
+def get_pull_logs(client, repository, tag=None, all_tags=False,  **kwargs):
+    repository, tag = get_repo_name_and_tag(repository, tag)
+    pull_log = client.api.pull(repository, tag=tag, stream=True, all_tags=all_tags, decode=True, **kwargs)
+    for log in pull_log:
+        yield log
+
+
+def get_image(client, repository, tag=None, all_tags=False):
+    repository, tag = get_repo_name_and_tag(repository, tag)
+    name = f'{0}{2}{1}'.format(repository, tag, '@' if tag.startswith('sha256:') else ':')
+
+    if not all_tags:
+        return client.images.get(name)
+    return client.list(repository)
+
+
 def install(agent_key: str, tag: str = '') -> None:
     """Install an agent : Fetch the docker file location of the agent corresponding to the agent_key,
     and pull the image from the registry.
@@ -39,12 +70,27 @@ def install(agent_key: str, tag: str = '') -> None:
         agent_details = response['data']['agent']
         agent_docker_location = agent_details['dockerLocation']
 
+    def is_image_present(agent_key):
+        agent_key = agent_key.replace('/', '_').lower()
+        docker_client = docker.from_env()
+        try:
+            docker_client.images.get(agent_key)
+            return True
+        except docker.errors.ImageNotFound:
+            return False
+
     try:
-        with console.status('Pulling the image from the registery.'):
-            docker_client = docker.from_env()
-            agent_image = docker_client.images.pull(agent_docker_location)
-            agent_key = agent_key.replace('/', '_').lower()
-            agent_image.tag(repository=agent_key, tag=tag)
+        if is_image_present(agent_key):
+            console.status(f'{agent_key} already exist.')
+
+        else:
+            with console.status('Pulling the image from the ostorlab store.'):
+                docker_client = docker.from_env()
+                pull_logs_generator = get_pull_logs(docker_client, agent_docker_location)
+                console.progress(pull_logs_generator)
+                agent_image = get_image(docker_client, agent_docker_location, tag)
+                agent_key = agent_key.replace('/', '_').lower()
+                agent_image.tag(repository=agent_key, tag=tag)
 
     except docker.errors.ImageNotFound as e:
         console.error(f'The provided agent key : {agent_key} does not seem to be public.')
