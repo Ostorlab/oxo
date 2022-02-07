a66a6ec8a9879135e13e104e95b8cb6dcbd98a28
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-02-07 23:38:03 +0100

    Install CLI command.

--- src/ostorlab/cli/agent/install/install.py
+++ src/ostorlab/cli/agent/install/install.py
@@ -2,7 +2,7 @@
 
 import click
 
-from ostorlab.cli.agent import agent
+from ostorlab.cli.agent import agent as agent_command
 from ostorlab.cli import console as cli_console
 from ostorlab.cli import install_agent
 
@@ -10,11 +10,9 @@ from ostorlab.cli import install_agent
 console = cli_console.Console()
 
 
-@agent.command()
+@agent_command.command()
 @click.option('--agent', '-a', help='Agent key.', required=True)
 @click.option('--version', '-v', help='Agent version.', required=False)
 def install(agent: str, version: str = '') -> None:
-    """CLI command to install an agent : Fetch the docker file location of the agent corresponding to the agent_key,
-    and pull the image from the registry.
-    """
+    """Install an agent : pull the image from the ostorlab store."""
     install_agent.install(agent, version)
