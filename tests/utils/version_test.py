31a9d097b095ae34979937d7a8b178b196b409b8
chevalier3as <chevalier3as@gmail.com>
2022-02-10 11:49:24 +0100

    Fix linting issue.

--- tests/utils/version_test.py
+++ tests/utils/version_test.py
@@ -4,20 +4,20 @@ from ostorlab.utils import version
 
 def testVersionComparison_always_returnsCorrectValue():
     """Check version comparison."""
-    assert version.Version("100.9900.0") > version.Version("19.0.0")
+    assert version.Version('100.9900.0') > version.Version('19.0.0')
 
 
 def testVersionComparison2_always_returnsCorrectValue():
     """Check version comparison."""
-    assert version.Version("100.9900.0") > version.Version("1.0.0")
+    assert version.Version('100.9900.0') > version.Version('1.0.0')
 
 
 def testVersionComparison_whenEqual_returnsTrue():
     """Check version equality for two different objects."""
-    assert version.Version("100.9900.0") == version.Version("100.9900.0")
+    assert version.Version('100.9900.0') == version.Version('100.9900.0')
 
 
 def testVersionMax_always_returnsCorrectValue():
     """Check use with native comparison method max."""
-    assert max([version.Version("100.9900.0"), version.Version("19.0.0"), version.Version("1.0.0")]) == version.Version(
-        "100.9900.0")
+    assert max([version.Version('100.9900.0'), version.Version('19.0.0'), version.Version('1.0.0')]) == version.Version(
+        '100.9900.0')
