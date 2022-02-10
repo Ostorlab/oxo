73ee001b128bfeb197b7980d0586a2031f3053be
chevalier3as <chevalier3as@gmail.com>
2022-02-10 11:18:16 +0100

    Add missing docstrings.

--- src/ostorlab/utils/version.py
+++ src/ostorlab/utils/version.py
@@ -1,42 +1,50 @@
+"""Version utility class to make easy to compare semantic version strings."""
 import semver
 
+
 class Version:
+    """Version class that only accepts semantic and provides python comparison API."""
 
     def __init__(self, version: str) -> None:
+        """Init version."""
         semver.parse(version)
         self._version = version
 
     def __repr__(self):
+        """Version string representation."""
         return f'<Version {self._version}>'
 
     def __lt__(self, other):
+        """Pythonic comparison API."""
         if isinstance(other, Version):
             return semver.compare(self._version, other._version) < 0
         else:
             raise ValueError()
 
     def __le__(self, other):
+        """Pythonic comparison API."""
         if isinstance(other, Version):
             return semver.compare(self._version, other._version) <= 0
         else:
             raise ValueError()
 
     def __gt__(self, other):
+        """Pythonic comparison API."""
         if isinstance(other, Version):
             return semver.compare(self._version, other._version) > 0
         else:
             raise ValueError()
 
     def __ge__(self, other):
+        """Pythonic comparison API."""
         if isinstance(other, Version):
             return semver.compare(self._version, other._version) >= 0
         else:
             raise ValueError()
 
     def __eq__(self, other):
+        """Pythonic comparison API."""
         if isinstance(other, Version):
             return self._version == other._version
         else:
             raise ValueError()
-
-
--- tests/utils/version_test.py
+++ tests/utils/version_test.py
@@ -1,14 +1,23 @@
+"""Unit test for version helper class."""
 from ostorlab.utils import version
 
 
 def testVersionComparison_always_returnsCorrectValue():
+    """Check version comparison."""
     assert version.Version("100.9900.0") > version.Version("19.0.0")
 
+
 def testVersionComparison2_always_returnsCorrectValue():
+    """Check version comparison."""
     assert version.Version("100.9900.0") > version.Version("1.0.0")
 
+
 def testVersionComparison_whenEqual_returnsTrue():
+    """Check version equality for two different objects."""
     assert version.Version("100.9900.0") == version.Version("100.9900.0")
 
+
 def testVersionMax_always_returnsCorrectValue():
-    assert max([version.Version("100.9900.0"), version.Version("19.0.0"), version.Version("1.0.0")]) == version.Version("100.9900.0")
+    """Check use with native comparison method max."""
+    assert max([version.Version("100.9900.0"), version.Version("19.0.0"), version.Version("1.0.0")]) == version.Version(
+        "100.9900.0")
