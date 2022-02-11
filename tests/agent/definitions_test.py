bb9b4c8f9babdfc5beeca808d2ed2e9f8d5668fc
mohsinenar <mnarmouche@gmail.com>
2022-02-11 14:39:39 +0100

    add extra fields to agent definitions.py

--- tests/agent/definitions_test.py
+++ tests/agent/definitions_test.py
@@ -7,21 +7,7 @@ from ostorlab.agent import definitions
 def testAgentDefinitionFromYaml_whenYamlIsValid_returnsValidAgentDefinition():
     """Tests the creation of an agent definition from a valid yaml definition file."""
     valid_yaml_data = """
-            name: "agent1"
-            description: "Agent1 Description should be here"
-            image: "some/path/to/the/image"
-            source: "https://github.com/"
-            durability: "development"
-            restrictions: 
-            - "restriction1"
-            - "restriction2"
-            in_selectors: 
-            - "in_selector1"
-            - "in_selector2"
-            out_selectors:
-            - "out_selector1"
-            - "out_selector2"
-            restart_policy: "any"
+            ssss
         """
 
     yaml_data_file = io.StringIO(valid_yaml_data)
