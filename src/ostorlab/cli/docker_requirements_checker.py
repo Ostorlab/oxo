1e3fe4828090ade6b94e703c4923e47aa8305bdc
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-02-11 14:10:19 +0100

    Add docstrings.

--- src/ostorlab/cli/docker_requirements_checker.py
+++ src/ostorlab/cli/docker_requirements_checker.py
@@ -2,8 +2,12 @@
 
 import docker
 
-def is_docker_installed():
-    """Checks if docker is installed"""
+def is_docker_installed() -> bool:
+    """Checks if docker is installed
+
+    Returns:
+        True if docker is installed, else False
+    """
     try:
         _ = docker.from_env()
     except docker.errors.DockerException as e:
@@ -11,8 +15,12 @@ def is_docker_installed():
             return False
     return True
 
-def is_user_permitted():
-    """Check if the user got permissions to run docker."""
+def is_user_permitted() -> bool:
+    """Check if the user got permissions to run docker.
+
+    Returns:
+        True if user has permission to run docker, else False
+    """
     try:
         _ = docker.from_env()
     except docker.errors.DockerException as e:
@@ -20,16 +28,20 @@ def is_user_permitted():
             return False
     return True
 
-def is_swarm_initialized():
-    """Checks if docker swarm is initialized."""
+def is_swarm_initialized() -> bool:
+    """Checks if docker swarm is initialized.
+
+    Returns:
+        True if docker swarm is initialized, else False
+    """
     if is_user_permitted():
         docker_client = docker.from_env()
         if len(docker_client.swarm.attrs.keys()) == 0:
             return False
         return True
 
-def init_swarm():
-    """Initialize swarm"""
+def init_swarm() -> None:
+    """Initialize docker swarm"""
     if is_user_permitted():
         docker_client = docker.from_env()
         docker_client.swarm.init()
