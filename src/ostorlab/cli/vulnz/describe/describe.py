23c8a94010695a64510490a182d9281c2b78e208
chevalier3as <chevalier3as@gmail.com>
2022-02-28 18:15:17 +0100

    Describe all vulnz of a single scan.

--- src/ostorlab/cli/vulnz/describe/describe.py
+++ src/ostorlab/cli/vulnz/describe/describe.py
@@ -1,6 +1,7 @@
 """Vulnz Describe command."""
 import logging
 import click
+from typing import Optional
 
 import rich
 from rich import markdown
@@ -16,28 +17,19 @@ console = cli_console.Console()
 
 logger = logging.getLogger(__name__)
 
-
-@vulnz.command(name='describe')
-@click.option('--vuln-id', '-v', 'vuln_id', help='Id of the vulnerability.', required=True)
-def describe_cli(vuln_id: int) -> None:
-    """CLI command to describe a vulnerability."""
-    database = models.Database()
-    session = database.session
-    vulnerability = session.query(models.Vulnerability).get(vuln_id)
+def _print_vulnerability(vulnerability):
+    """Print vulnerability details"""
     if vulnerability is None:
-        console.error(f'No vulnerability found matching id {vuln_id}')
-        raise click.exceptions.Exit(2)
+        return
 
-    console.success('Vulnerabilities retrieved successfully.')
     vulnz_list = [
         {'id': str(vulnerability.id),
-            'risk_rating': styles.style_risk(vulnerability.risk_rating.value.upper()),
-            'cvss_v3_vector': vulnerability.cvss_v3_vector,
-            'title': vulnerability.title,
-            'short_description': markdown.Markdown(vulnerability.short_description),
+         'risk_rating': styles.style_risk(vulnerability.risk_rating.value.upper()),
+         'cvss_v3_vector': vulnerability.cvss_v3_vector,
+         'title': vulnerability.title,
+         'short_description': markdown.Markdown(vulnerability.short_description),
          }
     ]
-
     columns = {
         'Id': 'id',
         'Title': 'title',
@@ -45,9 +37,34 @@ def describe_cli(vuln_id: int) -> None:
         'CVSS V3 Vector': 'cvss_v3_vector',
         'Short Description': 'short_description',
     }
-    title = f'Describing vulnerability {vuln_id}'
+    title = f'Describing vulnerability {vulnerability.id}'
     console.table(columns=columns, data=vulnz_list, title=title)
     rich.print(panel.Panel(markdown.Markdown(vulnerability.description), title='Description'))
     rich.print(panel.Panel(markdown.Markdown(vulnerability.recommendation), title='Recommendation'))
     rich.print(panel.Panel(markdown.Markdown(vulnerability.technical_detail), title='Technical details'))
+
+
+@vulnz.command(name='describe')
+@click.option('--vuln-id', '-v', 'vuln_id', help='Id of the vulnerability.', required=False)
+@click.option('--scan-id', '-s', 'scan_id', help='Id of the scan.', required=False)
+def describe_cli(vuln_id: Optional[int] = None, scan_id: Optional[int] = None) -> None:
+    """CLI command to describe a vulnerability."""
+    if vuln_id is not None:
+        database = models.Database()
+        session = database.session
+        vulnerability = session.query(models.Vulnerability).get(vuln_id)
+        console.success('Vulnerabilities retrieved successfully.')
+        _print_vulnerability(vulnerability)
+    elif scan_id is not None:
+        database = models.Database()
+        session = database.session
+        vulnerabilities = session.query(models.Vulnerability).filter_by(scan_id=scan_id).\
+            order_by(models.Vulnerability.title).all()
+        for v in vulnerabilities:
+            _print_vulnerability(v)
+        console.success('Vulnerabilities listed successfully.')
+
+
+
+
 
