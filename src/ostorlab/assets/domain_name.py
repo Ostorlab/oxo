7fd7a2652a097ed82c03194f02c62c9bb3151787
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-03-02 17:40:04 +0100

    fix selector in domain name asset.

--- src/ostorlab/assets/domain_name.py
+++ src/ostorlab/assets/domain_name.py
@@ -5,7 +5,7 @@ from ostorlab.assets import asset
 
 
 @dataclasses.dataclass
-@asset.selector('v3.asset.dns.a_record')
+@asset.selector('v3.asset.domain_name')
 class DomainName(asset.Asset):
     """Domain Name target asset per RFC 1034 and 1035."""
     name: str
