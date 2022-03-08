0100a4c1810411ec6978ef3b7fe62c6e8ac76686
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-03-08 18:03:14 +0100

    Create agent group api request.

---
+++ src/ostorlab/apis/agent_group.py
@@ -0,0 +1,55 @@
+"""Create Agent Group with via an API Request."""
+import json
+from typing import Any, Dict, List, Optional
+
+from ostorlab.apis import request
+
+
+class CreateAgentGroupAPIRequest(request.APIRequest):
+    """Persist agent group API request."""
+
+    def __init__(self, name: str, description: str, agents: List[Dict[str:Any]]) -> None:
+        """Initializer"""
+        self._name = name
+        self._description = description
+        self._agents = agents
+
+    @property
+    def query(self) -> Optional[str]:
+        """Sets the query of the API request.
+
+        Returns:
+            The query to create the agent group.
+        """
+        return """
+            query publishAgentGroup($agentGroup: AgentGroupCreateInputType!){
+                publishAgentGroup(agentKeys: $agentKeys) {
+                    agentGroup{
+                        key
+                    }
+                }
+            }
+        """
+
+
+    @property
+    def data(self) -> Optional[Dict]:
+        """Sets the body of the API request.
+
+        Returns:
+            The body of the create agent group request.
+        """
+
+        variables = {
+            'agentGroup': {
+                'name': self._name,
+                'description': self._description,
+                'access': 'PRIVATE',
+                'agents': self._agents
+            }
+        }
+        data = {
+            'query': self.query,
+            'variables': json.dumps(variables)
+        }
+        return data
