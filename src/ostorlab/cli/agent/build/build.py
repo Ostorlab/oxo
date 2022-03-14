daab6926bae2adc3557ea1420d7c4e95985cb9c9
chevalier3as <chevalier3as@gmail.com>
2022-03-14 19:15:12 +0100

    Add agent build force flag to delete existing images.

--- src/ostorlab/cli/agent/build/build.py
+++ src/ostorlab/cli/agent/build/build.py
@@ -18,10 +18,40 @@ console = cli_console.Console()
 logger = logging.getLogger(__name__)
 
 
+def _image_exists(image: str) -> bool:
+    try:
+        docker_sdk_client = docker.from_env()
+        docker_sdk_client.images.get(image)
+        return True
+    except docker.errors.ImageNotFound:
+        return False
+
+
+def _build_image(agent_name, container_name, dockerfile_path, docker_build_root, agent_definition_file):
+    console.info(
+        f'Building agent [bold red]{agent_name}[/] dockerfile [bold red]{dockerfile_path}[/]'
+        f' at root [bold red]{docker_build_root}[/].')
+    with console.status(f'Building [bold red]{container_name}[/]'):
+        for log in build_progress.BuildProgress().build(path=docker_build_root,
+                                                        dockerfile=dockerfile_path,
+                                                        tag=container_name,
+                                                        labels={'agent_definition': agent_definition_file.read().decode(
+                                                            'utf-8')}):
+
+            if 'stream' in log and log['stream'] != '\n':
+                console.info(log['stream'][:-1])
+            elif 'error' in log:
+                console.error(log['error'][:-1])
+            else:
+                logger.debug(log)
+
+    console.success(f'Agent {agent_name} built, container [bold red]{container_name}[/] created.')
+
 @agent.command()
 @click.option('--file', '-f', type=click.File('rb'), help='Path to Agent yaml definition.', required=True)
 @click.option('--organization', '-o', help='Organization name.', required=False, default='')
-def build(file: io.FileIO, organization: str = '') -> None:
+@click.option('--force/--no-force', default=False)
+def build(file: io.FileIO, organization: str = '', force: bool = False) -> None:
     """CLI command to build the agent container from a definition.yaml file.
     Usage : Ostorlab agent build -f path/to/definition.yaml -org organization_name
     """
@@ -29,51 +59,38 @@ def build(file: io.FileIO, organization: str = '') -> None:
     if not docker_requirements_checker.is_docker_installed():
         console.error('Docker is not installed.')
         raise click.exceptions.Exit(2)
-    elif not docker_requirements_checker.is_user_permitted():
+    if not docker_requirements_checker.is_user_permitted():
         console.error('User does not have permissions to run docker.')
         raise click.exceptions.Exit(2)
-    elif not docker_requirements_checker.is_docker_working():
+    if not docker_requirements_checker.is_docker_working():
         console.error('Error using docker.')
         raise click.exceptions.Exit(2)
-    else:
-        try:
-            agent_def = loader.load_agent_yaml(file)
-            file.seek(0)
-            dockerfile_path = agent_def['docker_file_path']
-            docker_build_root = agent_def['docker_build_root']
-            agent_name = agent_def['name']
-            agent_version = agent_def.get('version', '0.0.0')
-            container_name = f'agent_{organization}_{agent_name}:v{agent_version}'
-            docker_sdk_client = docker.from_env()
 
-            try:
-                docker_sdk_client.images.get(container_name)
+    try:
+        agent_def = loader.load_agent_yaml(file)
+        file.seek(0)
+        dockerfile_path = agent_def['docker_file_path']
+        docker_build_root = agent_def['docker_build_root']
+        agent_name = agent_def['name']
+        agent_version = agent_def.get('version', '0.0.0')
+        container_name = f'agent_{organization}_{agent_name}:v{agent_version}'
+        client = docker.from_env()
+
+        if _image_exists(container_name):
+            if force is False:
                 console.info(f'{container_name} already exist.')
                 raise click.exceptions.Exit(0)
-            except docker.errors.ImageNotFound:
-                console.info(
-                    f'Building agent [bold red]{agent_name}[/] dockerfile [bold red]{dockerfile_path}[/]'
-                    f' at root [bold red]{docker_build_root}[/].')
-                with console.status(f'Building [bold red]{container_name}[/]'):
-                    for log in build_progress.BuildProgress().build(path=docker_build_root,
-                                                                    dockerfile=dockerfile_path,
-                                                                    tag=container_name,
-                                                                    labels={'agent_definition': file.read().decode(
-                                                                        'utf-8')}):
+            else:
+                console.warning(f'{container_name} already exist, deleting image.')
+                client.images.remove(container_name, force=True)
 
-                        if 'stream' in log and log['stream'] != '\n':
-                            console.info(log['stream'][:-1])
-                        elif 'error' in log:
-                            console.error(log['error'][:-1])
-                        else:
-                            logger.debug(log)
+        _build_image(agent_name, container_name, dockerfile_path, docker_build_root, file)
 
-                console.success(f'Agent {agent_name} built, container [bold red]{container_name}[/] created.')
-        except errors.BuildError:
-            console.error('Error building agent.')
-        except validator.SchemaError:
-            console.error(
-                'Schema is invalid, this should not happen, please report an issue at '
-                'https://github.com/Ostorlab/ostorlab/issues.')
-        except validator.ValidationError:
-            console.error('Definition file does not conform to the provided specification.')
+    except errors.BuildError:
+        console.error('Error building agent.')
+    except validator.SchemaError:
+        console.error(
+            'Schema is invalid, this should not happen, please report an issue at '
+            'https://github.com/Ostorlab/ostorlab/issues.')
+    except validator.ValidationError:
+        console.error('Definition file does not conform to the provided specification.')
--- tests/cli/agent/build/build_test.py
+++ tests/cli/agent/build/build_test.py
@@ -1,17 +1,17 @@
 """Tests for CLI agent build command."""
 
-import pytest
-import docker
 from pathlib import Path
 
+import docker
+import pytest
 from click import testing
 
 from ostorlab.cli import rootcli
 
 
 def testAgentBuildCLI_whenRequiredOptionFileIsMissing_showMessage():
-    """Test ostorlab agent build CLI command without the required file option.
-    Should show help message, and confirm the --file option is missing.
+    """Test ostorlab agent build CLI command without the required file option. Should show help message, and confirm
+    the --file option is missing.
     """
     runner = testing.CliRunner()
 
@@ -34,37 +34,58 @@ def _is_docker_image_present(image: str):
 @pytest.mark.docker
 @pytest.mark.parametrize('image_cleanup', ['dummy'], indirect=True)
 def testAgentBuildCLI_whenCommandIsValid_buildCompletedAndNoRaiseImageNotFoundExcep(image_cleanup):
-    """Test ostorlab agent build CLI command : Case where the command is valid.
-    The agent container should be built.
+    """Test ostorlab agent build CLI command : Case where the command is valid. The agent container should be built.
     """
     del image_cleanup
     dummy_def_yaml_file_path = Path(__file__).parent / 'assets/dummydef.yaml'
     runner = testing.CliRunner()
     _ = runner.invoke(rootcli.rootcli, [
-                                        'agent',
-                                        'build',
-                                        f'--file={dummy_def_yaml_file_path}',
-                                        '--organization=ostorlab'
-                                        ])
+        'agent',
+        'build',
+        f'--file={dummy_def_yaml_file_path}',
+        '--organization=ostorlab'
+    ])
     assert _is_docker_image_present('agent_ostorlab_dummy_agent:v1.0.0') is True
 
 
 @pytest.mark.docker
 @pytest.mark.parametrize('image_cleanup', ['dummy'], indirect=True)
-def testAgentBuildCLI_whenCommandIsValidAndImageAlreadyExists_ShowsMessageAndExists(image_cleanup):
-    """Test ostorlab agent build CLI command : Case where the command is valid.
-    The agent container should be built.
+def testAgentBuildCLI_whenCommandIsValidAndImageAlreadyExists_showsMessageAndExists(image_cleanup):
+    """Test ostorlab agent build CLI command : Case where the command is valid. The agent container should be built.
     """
     del image_cleanup
-    dummy_def_yaml_file_path =  Path(__file__).parent / 'assets/dummydef.yaml'
+    dummy_def_yaml_file_path = Path(__file__).parent / 'assets/dummydef.yaml'
     runner = testing.CliRunner()
     _ = runner.invoke(rootcli.rootcli, ['agent',
-                                              'build',
-                                              f'--file={dummy_def_yaml_file_path}',
-                                              '--organization=ostorlab'])
+                                        'build',
+                                        f'--file={dummy_def_yaml_file_path}',
+                                        '--organization=ostorlab'])
     result = runner.invoke(rootcli.rootcli, ['agent',
                                              'build',
                                              f'--file={dummy_def_yaml_file_path}',
                                              '--organization=ostorlab'])
     assert 'already exist' in result.output
+    assert result.exit_code == 0
+
+
+@pytest.mark.docker
+@pytest.mark.parametrize('image_cleanup', ['dummy'], indirect=True)
+def testAgentBuildCLI_whenCommandIsValidAndImageAlreadyExistsAndForceFlagPassed_buildCompletedAndNoRaiseImageNotFoundExcep(
+        image_cleanup):
+    """Test ostorlab agent build CLI command : Case where the command is valid, the image exists and force flag is
+     passed. The agent container should be built.
+    """
+    del image_cleanup
+    dummy_def_yaml_file_path = Path(__file__).parent / 'assets/dummydef.yaml'
+    runner = testing.CliRunner()
+    _ = runner.invoke(rootcli.rootcli, ['agent',
+                                        'build',
+                                        f'--file={dummy_def_yaml_file_path}',
+                                        '--organization=ostorlab'])
+    result = runner.invoke(rootcli.rootcli, ['agent',
+                                             'build',
+                                             '--force',
+                                             f'--file={dummy_def_yaml_file_path}',
+                                             '--organization=ostorlab'])
+    assert 'already exist' not in result.output
     assert result.exit_code == 0
