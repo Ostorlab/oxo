641027a2022f384537a7a2a3f31b8e10bc89844f
chevalier3as <chevalier3as@gmail.com>
2022-04-04 13:34:39 +0000

    Add defensive checks to the mixin.

--- src/ostorlab/agent/mixins/agent_persist_mixin.py
+++ src/ostorlab/agent/mixins/agent_persist_mixin.py
@@ -19,6 +19,13 @@ from ostorlab.runtimes import definitions as runtime_definitions
 class AgentPersistMixin:
 
     def __init__(self, agent_settings: runtime_definitions.AgentSettings) -> None:
+        """Initializes the mixin from the agent settings.
+
+        Args:
+            agent_settings:
+        """
+        if agent_settings.redis_url is None:
+            raise ValueError('agent settings is missing redis url')
         self._redis_client = redis.Redis.from_url(agent_settings.redis_url)
 
     def set_add(self, key, value) -> bool:
