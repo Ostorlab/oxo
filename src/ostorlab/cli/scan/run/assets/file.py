0595fb5249ac8abcd8f2c2b0c8d7b2a24764f47a
mouhcine narhmouche <mnarmouche@gmail.com>
2022-04-12 14:32:43 +0000

    Update src/ostorlab/cli/scan/run/assets/file.py
    Co-authored-by: Alaeddine Mesbahi <alaeddine.mesbahi@ostorlab.dev>

--- src/ostorlab/cli/scan/run/assets/file.py
+++ src/ostorlab/cli/scan/run/assets/file.py
@@ -21,5 +21,5 @@ def file_cli(ctx: click.core.Context, files: List[io.FileIO]) -> None:
     assets = []
     for f in files:
         assets.append(file_asset.File(content=f.read(), path=f.name))
-    logger.debug('scanning asset %s', [str(asset) for asset in assets])
+    logger.debug('scanning assets %s', [str(asset) for asset in assets])
     runtime.scan(title=ctx.obj['title'], agent_group_definition=ctx.obj['agent_group_definition'], assets=assets)
