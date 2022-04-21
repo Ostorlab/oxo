60a3f8bfbc90845974407e1157269fc1d6868201
asasas333_3 <am.mesbahi@gmail.com>
2022-04-21 21:06:48 +0000

    Add inject asset link

---
+++ src/ostorlab/assets/link.py
@@ -0,0 +1,16 @@
+"""Link asset."""
+import dataclasses
+from typing import Optional
+
+from ostorlab.assets import asset
+
+
+@dataclasses.dataclass
+@asset.selector('v3.asset.link')
+class Link(asset.Asset):
+    """Agent asset."""
+    url: str
+    method: str
+
+    def __str__(self):
+        return f'Link {self.url} with method {self.method}'
