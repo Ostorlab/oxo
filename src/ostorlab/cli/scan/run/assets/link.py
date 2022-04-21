e40f941328a015dbec5fd0b2cbe797d06beac333
asasas333_3 <am.mesbahi@gmail.com>
2022-04-21 21:11:07 +0000

    fix class name

--- src/ostorlab/cli/scan/run/assets/link.py
+++ src/ostorlab/cli/scan/run/assets/link.py
@@ -3,7 +3,7 @@ import logging
 
 import click
 
-from ostorlab.assets import link
+from ostorlab.assets import link as link_asset
 from ostorlab.cli.scan.run import run
 
 logger = logging.getLogger(__name__)
@@ -16,6 +16,6 @@ logger = logging.getLogger(__name__)
 def link(ctx: click.core.Context, url: str, method: str) -> None:
     """Run scan for link."""
     runtime = ctx.obj['runtime']
-    asset = link.Link(url=url, method=method)
+    asset = link_asset.Link(url=url, method=method)
     logger.debug('scanning asset %s', asset)
     runtime.scan(title=ctx.obj['title'], agent_group_definition=ctx.obj['agent_group_definition'], assets=[asset])
