1268951968d5f6e11fcd3785051f5f0609d53ef9
asasas333_3 <am.mesbahi@gmail.com>
2022-04-27 10:25:19 +0000

    Add exit code

--- src/ostorlab/cli/agent/build/build.py
+++ src/ostorlab/cli/agent/build/build.py
@@ -92,9 +92,12 @@ def build(file: io.FileIO, organization: str = '', force: bool = False, no_cache
 
     except errors.BuildError:
         console.error('Error building agent.')
+        raise click.exceptions.Exit(2)
     except validator.SchemaError:
         console.error(
             'Schema is invalid, this should not happen, please report an issue at '
             'https://github.com/Ostorlab/ostorlab/issues.')
+        raise click.exceptions.Exit(2)
     except validator.ValidationError:
         console.error('Definition file does not conform to the provided specification.')
+        raise click.exceptions.Exit(2)
