2e4c36665e7ce74db465dff1d7d1d1a47a7cfa52
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-07-19 18:17:15 +0100

    Add email field to IP whois protobuff message.

--- src/ostorlab/agent/message/proto/v3/asset/ip/v6/whois/whois_pb2.py
+++ src/ostorlab/agent/message/proto/v3/asset/ip/v6/whois/whois_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='v3.asset.ip.v6.whois',
   syntax='proto2',
   serialized_options=None,
-  serialized_pb=_b('\n v3/asset/ip/v6/whois/whois.proto\x12\x14v3.asset.ip.v6.whois\"L\n\x07Network\x12\x0c\n\x04\x63idr\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06handle\x18\x03 \x01(\t\x12\x15\n\rparent_handle\x18\x04 \x01(\t\"6\n\x07\x43ontact\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04kind\x18\x02 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x03 \x01(\t\"F\n\x06\x45ntity\x12\x0c\n\x04name\x18\x01 \x01(\t\x12.\n\x07\x63ontact\x18\x02 \x01(\x0b\x32\x1d.v3.asset.ip.v6.whois.Contact\"\x88\x02\n\x07Message\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\t\x12\x12\n\x07version\x18\x03 \x01(\x05:\x01\x36\x12\x14\n\x0c\x61sn_registry\x18\x04 \x01(\t\x12\x12\n\nasn_number\x18\x05 \x01(\x05\x12\x18\n\x10\x61sn_country_code\x18\x06 \x01(\t\x12\x10\n\x08\x61sn_date\x18\x07 \x01(\t\x12\x17\n\x0f\x61sn_description\x18\x08 \x01(\t\x12.\n\x07network\x18\t \x01(\x0b\x32\x1d.v3.asset.ip.v6.whois.Network\x12.\n\x08\x65ntities\x18\n \x03(\x0b\x32\x1c.v3.asset.ip.v6.whois.Entity')
+  serialized_pb=_b('\n v3/asset/ip/v6/whois/whois.proto\x12\x14v3.asset.ip.v6.whois\"L\n\x07Network\x12\x0c\n\x04\x63idr\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06handle\x18\x03 \x01(\t\x12\x15\n\rparent_handle\x18\x04 \x01(\t\"E\n\x07\x43ontact\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04kind\x18\x02 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x03 \x01(\t\x12\r\n\x05\x65mail\x18\x04 \x01(\t\"F\n\x06\x45ntity\x12\x0c\n\x04name\x18\x01 \x01(\t\x12.\n\x07\x63ontact\x18\x02 \x01(\x0b\x32\x1d.v3.asset.ip.v6.whois.Contact\"\x88\x02\n\x07Message\x12\x0c\n\x04host\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\t\x12\x12\n\x07version\x18\x03 \x01(\x05:\x01\x36\x12\x14\n\x0c\x61sn_registry\x18\x04 \x01(\t\x12\x12\n\nasn_number\x18\x05 \x01(\x05\x12\x18\n\x10\x61sn_country_code\x18\x06 \x01(\t\x12\x10\n\x08\x61sn_date\x18\x07 \x01(\t\x12\x17\n\x0f\x61sn_description\x18\x08 \x01(\t\x12.\n\x07network\x18\t \x01(\x0b\x32\x1d.v3.asset.ip.v6.whois.Network\x12.\n\x08\x65ntities\x18\n \x03(\x0b\x32\x1c.v3.asset.ip.v6.whois.Entity')
 )
 
 
@@ -105,6 +105,13 @@ _CONTACT = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='email', full_name='v3.asset.ip.v6.whois.Contact.email', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -118,7 +125,7 @@ _CONTACT = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=136,
-  serialized_end=190,
+  serialized_end=205,
 )
 
 
@@ -155,8 +162,8 @@ _ENTITY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=192,
-  serialized_end=262,
+  serialized_start=207,
+  serialized_end=277,
 )
 
 
@@ -249,8 +256,8 @@ _MESSAGE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=265,
-  serialized_end=529,
+  serialized_start=280,
+  serialized_end=544,
 )
 
 _ENTITY.fields_by_name['contact'].message_type = _CONTACT
