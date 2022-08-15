c5a120ea97c80c3c9a34f4c5598a1493e9b67058
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-08-15 17:06:05 +0100

    Add unit tests for the dict mionifier utils.

---
+++ tests/utils/dictionary_minifier_test.py
@@ -0,0 +1,83 @@
+"""Unit test for the dictionary minifier module."""
+from ostorlab.utils import dictionary_minifier
+
+
+def testMinifyDict_whenSimpleDict_shouldMinifyStringValues():
+    """Ensure the minify dict method return correct values for a simple dictionary."""
+    input_dict = {
+        'key1': 'very long string value.....',
+        'key2': 'another very long string value.....',
+        'key3': 'a third very long string value.....',
+    }
+    dictionary_minifier.TRUNCATE_SIZE = 5
+
+    minified_dict = dictionary_minifier.minify_dict(input_dict, dictionary_minifier.truncate_str)
+
+    assert minified_dict == {
+        'key1': 'very ',
+        'key2': 'anoth',
+        'key3': 'a thi',
+    }
+
+
+def testMinifyDict_whenNestedDict_shouldMinifyStringValues():
+    """Ensure the minify dict method return correct values for nested dictionaries."""
+    input_dict = {
+        'key1': 'very long string value.....',
+        'key2': {
+            'key3': {
+                'key4': 'key4 very long string value....'
+            },
+            'key5': 5
+        },
+        'key6': b'a third very long string value.....',
+    }
+    dictionary_minifier.TRUNCATE_SIZE = 2
+
+    minified_dict = dictionary_minifier.minify_dict(input_dict, dictionary_minifier.truncate_str)
+
+    assert minified_dict == {
+        'key1': 've',
+        'key2': {
+            'key3': {
+                'key4': 'ke'
+            },
+            'key5': 5
+        },
+        'key6': b'a ',
+    }
+
+
+def testMinifyDict_whenNestedDictsAndList_shouldMinifyStringValues():
+    """Ensure the minify dict method return correct values for nested dictionaries and lists."""
+    input_dict = {
+        'key1': 'very long string value.....',
+        'key2': {
+            'key3': {
+                'listValues': [
+                    {'key4': 'key4 very long string value....'},
+                    {'key5': b'key5 very long string value....'},
+                    {'key6': 42}
+                ]
+            },
+            'key5': 42
+        },
+        'key6': b'a third very long string value.....',
+    }
+    dictionary_minifier.TRUNCATE_SIZE = 3
+    minified_dict = dictionary_minifier.minify_dict(input_dict, dictionary_minifier.truncate_str)
+
+    assert minified_dict == {
+        'key1': 'ver',
+        'key2': {
+            'key3': {
+                'listValues': [
+                    {'key4': 'key'},
+                    {'key5': b'key'},
+                    {'key6': 42},
+                ]
+            },
+            'key5': 42
+        },
+        'key6': b'a t',
+    }
