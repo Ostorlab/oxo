bdf370e8b6f3026062c888100767ed660448c675
chevalier3as <chevalier3as@gmail.com>
2022-08-16 09:48:27 +0100

    Fix tmpfile context management.

--- tests/agent/mixins/agent_open_telemetry_test.py
+++ tests/agent/mixins/agent_open_telemetry_test.py
@@ -22,70 +22,70 @@ def _is_windows() -> bool:
     return sys.platform == 'win32'
 
 def testOpenTelemetryMixin_whenEmitMessage_shouldTraceMessage(agent_mock):
-    """Unit test for the OpenTelemtry Mixin, ensure the correct exporter has been used and trace span has been sent."""
+    """Unit test for the OpenTelemetry Mixin, ensure the correct exporter has been used and trace span has been sent."""
     del agent_mock
-    tmp_file_obj = tempfile.NamedTemporaryFile(suffix='.json') # pylint: disable=R1732
-    if _is_windows() is True:
-        output_path = tmp_file_obj.name[2:].replace('\\', '/')
-    else:
-        output_path = tmp_file_obj.name
-    agent_definition = agent_definitions.AgentDefinition(
-        name='some_name',
-        out_selectors=['v3.report.vulnerability'])
-    agent_settings = runtime_definitions.AgentSettings(
-        key='some_key',
-        tracing_collector_url=f'file://{output_path}')
-    test_agent = TestAgent(
-        agent_definition=agent_definition,
-        agent_settings=agent_settings)
+    with tempfile.NamedTemporaryFile(suffix='.json') as tmp_file_obj:
+        if _is_windows() is True:
+            output_path = tmp_file_obj.name[2:].replace('\\', '/')
+        else:
+            output_path = tmp_file_obj.name
+        agent_definition = agent_definitions.AgentDefinition(
+            name='some_name',
+            out_selectors=['v3.report.vulnerability'])
+        agent_settings = runtime_definitions.AgentSettings(
+            key='some_key',
+            tracing_collector_url=f'file://{output_path}')
+        test_agent = TestAgent(
+            agent_definition=agent_definition,
+            agent_settings=agent_settings)
 
-    test_agent.emit('v3.report.vulnerability', {
-        'title': 'some_title',
-        'technical_detail': 'some_details',
-        'risk_rating': 'MEDIUM'
-    })
-    test_agent.force_flush_file_exporter()
+        test_agent.emit('v3.report.vulnerability', {
+            'title': 'some_title',
+            'technical_detail': 'some_details',
+            'risk_rating': 'MEDIUM'
+        })
+        test_agent.force_flush_file_exporter()
 
-    with open(output_path, 'r', encoding='utf-8') as trace_file:
-        trace_content = trace_file.read()
-        trace_object = json.loads(trace_content)
+        with open(output_path, 'r', encoding='utf-8') as trace_file:
+            trace_content = trace_file.read()
+            trace_object = json.loads(trace_content)
 
-        assert trace_object['name'] == 'emit_message'
-        assert trace_object['attributes']['agent.name'] == 'some_name'
-        assert trace_object['attributes']['message.selector'] == 'v3.report.vulnerability'
+            assert trace_object['name'] == 'emit_message'
+            assert trace_object['attributes']['agent.name'] == 'some_name'
+            assert trace_object['attributes']['message.selector'] == 'v3.report.vulnerability'
 
 
 def testOpenTelemetryMixin_whenProcessMessage_shouldTraceMessage(agent_mock):
     """Unit test for the OpenTelemtry Mixin, ensure the correct exporter has been used and trace span has been sent."""
     del agent_mock
-    tmp_file_obj = tempfile.NamedTemporaryFile(suffix='.json') # pylint: disable=R1732
-    if _is_windows() is True:
-        output_path = tmp_file_obj.name[2:].replace('\\', '/')
-    else:
-        output_path = tmp_file_obj.name
-    agent_definition = agent_definitions.AgentDefinition(
-        name='some_name',
-        in_selectors=['v3.report.vulnerability'])
-    agent_settings = runtime_definitions.AgentSettings(
-        key='some_key',
-        tracing_collector_url=f'file://{output_path}')
-    test_agent = TestAgent(
-        agent_definition=agent_definition,
-        agent_settings=agent_settings)
-    data = {
-        'title': 'some_title',
-        'technical_detail': 'some_details',
-        'risk_rating': 'MEDIUM'
-    }
-    raw = serializer.serialize('v3.report.vulnerability', data).SerializeToString()
+    with tempfile.NamedTemporaryFile(suffix='.json') as tmp_file_obj:
+        if _is_windows() is True:
+            output_path = tmp_file_obj.name[2:].replace('\\', '/')
+        else:
+            output_path = tmp_file_obj.name
+        agent_definition = agent_definitions.AgentDefinition(
+            name='some_name',
+            in_selectors=['v3.report.vulnerability'])
+        agent_settings = runtime_definitions.AgentSettings(
+            key='some_key',
+            tracing_collector_url=f'file://{output_path}')
+        test_agent = TestAgent(
+            agent_definition=agent_definition,
+            agent_settings=agent_settings)
+        data = {
+            'title': 'some_title',
+            'technical_detail': 'some_details',
+            'risk_rating': 'MEDIUM'
+        }
+        raw = serializer.serialize('v3.report.vulnerability', data).SerializeToString()
 
-    test_agent.process_message(selector='v3.report.vulnerability', message=raw)
-    test_agent.force_flush_file_exporter()
+        test_agent.process_message(selector='v3.report.vulnerability', message=raw)
+        test_agent.force_flush_file_exporter()
 
-    with open(output_path, 'r', encoding='utf-8') as trace_file:
-        trace_content = trace_file.read()
-        trace_object = json.loads(trace_content)
+        with open(output_path, 'r', encoding='utf-8') as trace_file:
+            trace_content = trace_file.read()
+            trace_object = json.loads(trace_content)
 
-        assert trace_object['name'] == 'process_message'
-        assert trace_object['attributes']['agent.name'] == 'some_name'
-        assert trace_object['attributes']['message.selector'] == 'v3.report.vulnerability'
+            assert trace_object['name'] == 'process_message'
+            assert trace_object['attributes']['agent.name'] == 'some_name'
+            assert trace_object['attributes']['message.selector'] == 'v3.report.vulnerability'
