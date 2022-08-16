de3261723a06b73ae5c18a021dad123912ba6f42
chevalier3as <chevalier3as@gmail.com>
2022-08-16 10:22:54 +0100

    Skip test as not relevant to windows, only runs within the agent docker.

--- tests/agent/mixins/agent_open_telemetry_test.py
+++ tests/agent/mixins/agent_open_telemetry_test.py
@@ -2,6 +2,7 @@
 import json
 import sys
 import tempfile
+import pytest
 
 from ostorlab.agent import agent
 from ostorlab.agent import definitions as agent_definitions
@@ -17,26 +18,17 @@ class TestAgent(agent.Agent):
         pass
 
 
-def _is_windows() -> bool:
-    """Returns true if current platform is windows."""
-    return sys.platform == 'win32'
-
-
+@pytest.skipif(sys.platform == "win32", reason="does not run on windows")
 def testOpenTelemetryMixin_whenEmitMessage_shouldTraceMessage(agent_mock):
     """Unit test for the OpenTelemetry Mixin, ensure the correct exporter has been used and trace span has been sent."""
     del agent_mock
     with tempfile.NamedTemporaryFile(suffix='.json') as tmp_file_obj:
-        if _is_windows() is True:
-            print(tmp_file_obj.name)
-            output_path = tmp_file_obj.name[2:].replace('\\', '/')
-        else:
-            output_path = tmp_file_obj.name
         agent_definition = agent_definitions.AgentDefinition(
             name='some_name',
             out_selectors=['v3.report.vulnerability'])
         agent_settings = runtime_definitions.AgentSettings(
             key='some_key',
-            tracing_collector_url=f'file://{output_path}')
+            tracing_collector_url=f'file://{tmp_file_obj.name}')
         test_agent = TestAgent(
             agent_definition=agent_definition,
             agent_settings=agent_settings)
@@ -48,7 +40,7 @@ def testOpenTelemetryMixin_whenEmitMessage_shouldTraceMessage(agent_mock):
         })
         test_agent.force_flush_file_exporter()
 
-        with open(output_path, 'r', encoding='utf-8') as trace_file:
+        with open(tmp_file_obj.name, 'r', encoding='utf-8') as trace_file:
             trace_content = trace_file.read()
             trace_object = json.loads(trace_content)
 
@@ -57,21 +49,17 @@ def testOpenTelemetryMixin_whenEmitMessage_shouldTraceMessage(agent_mock):
             assert trace_object['attributes']['message.selector'] == 'v3.report.vulnerability'
 
 
+@pytest.skipif(sys.platform == "win32", reason="does not run on windows")
 def testOpenTelemetryMixin_whenProcessMessage_shouldTraceMessage(agent_mock):
     """Unit test for the OpenTelemtry Mixin, ensure the correct exporter has been used and trace span has been sent."""
     del agent_mock
     with tempfile.NamedTemporaryFile(suffix='.json') as tmp_file_obj:
-        if _is_windows() is True:
-            print(tmp_file_obj.name)
-            output_path = tmp_file_obj.name[2:].replace('\\', '/')
-        else:
-            output_path = tmp_file_obj.name
         agent_definition = agent_definitions.AgentDefinition(
             name='some_name',
             in_selectors=['v3.report.vulnerability'])
         agent_settings = runtime_definitions.AgentSettings(
             key='some_key',
-            tracing_collector_url=f'file://{output_path}')
+            tracing_collector_url=f'file://{tmp_file_obj.name}')
         test_agent = TestAgent(
             agent_definition=agent_definition,
             agent_settings=agent_settings)
@@ -85,7 +73,7 @@ def testOpenTelemetryMixin_whenProcessMessage_shouldTraceMessage(agent_mock):
         test_agent.process_message(selector='v3.report.vulnerability', message=raw)
         test_agent.force_flush_file_exporter()
 
-        with open(output_path, 'r', encoding='utf-8') as trace_file:
+        with open(tmp_file_obj.name, 'r', encoding='utf-8') as trace_file:
             trace_content = trace_file.read()
             trace_object = json.loads(trace_content)
 
