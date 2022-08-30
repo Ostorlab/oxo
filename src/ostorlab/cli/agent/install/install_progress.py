7d1558546aba4c4c7cffe3c71cadf79bc2b6bbc3
oussama <o.essaji@ostorlab.dev>
2022-08-30 15:52:16 +0100

    Fix log status

--- src/ostorlab/cli/agent/install/install_progress.py
+++ src/ostorlab/cli/agent/install/install_progress.py
@@ -23,7 +23,7 @@ class AgentInstallProgress(progress.Progress):
                                    '•',
                                    progress.TimeRemainingColumn())
 
-    def display(self, logs_generator: Iterator[Dict]) -> None:
+    def display(self, logs_generator: Iterator[Dict], ) -> None:
         """Display the progress of the agent install command.
 
         Args:
@@ -34,30 +34,31 @@ class AgentInstallProgress(progress.Progress):
 
         with self as prg:
             for log in logs_generator:
-                if 'Downloading' in log['status']:
-                    task_id = log['id']
-                    if task_id not in download_tasks:
-                        task = prg.add_task(f'[red]Download : {task_id}',
-                                            total = log['progressDetail']['total'])
-                        download_tasks[task_id] = task
-                    task = download_tasks[task_id]
-                    current_progress = log['progressDetail']['current']
-                    prg.update(task, completed=current_progress)
-
-                elif 'Download complete' in log['status']:
-                    task_id = log['id']
-                    if task_id in download_tasks:
+                if log.get('status') is not None:
+                    if 'Downloading' in log.get('status'):
+                        task_id = log['id']
+                        if task_id not in download_tasks:
+                            task = prg.add_task(f'[red]Download : {task_id}',
+                                                total=log['progressDetail']['total'])
+                            download_tasks[task_id] = task
                         task = download_tasks[task_id]
-                        prg.update(task, total=100.0)
+                        current_progress = log['progressDetail']['current']
+                        prg.update(task, completed=current_progress)
 
-                elif 'Extracting' in log['status']:
-                    task_id = log['id']
-                    if task_id not in extract_tasks:
-                        task = prg.add_task(f'[red]Extract : {task_id}',
-                                            total = log['progressDetail']['total'])
-                        extract_tasks[task_id] = task
-                    task = extract_tasks[task_id]
-                    current_progress = log['progressDetail']['current']
-                    prg.update(task, completed=current_progress)
+                    elif 'Download complete' in log.get('status'):
+                        task_id = log['id']
+                        if task_id in download_tasks:
+                            task = download_tasks[task_id]
+                            prg.update(task, total=100.0)
+
+                    elif 'Extracting' in log.get('status'):
+                        task_id = log['id']
+                        if task_id not in extract_tasks:
+                            task = prg.add_task(f'[red]Extract : {task_id}',
+                                                total=log['progressDetail']['total'])
+                            extract_tasks[task_id] = task
+                        task = extract_tasks[task_id]
+                        current_progress = log['progressDetail']['current']
+                        prg.update(task, completed=current_progress)
 
         console.success('Installation successful')
