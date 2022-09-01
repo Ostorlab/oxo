355c12f388ad5bce7febc5c8ef4e5ecc57e575e3
chevalier3as <alaeddine.mesbahi@ostorlab.dev>
2022-09-01 09:45:17 +0100

    More typing fixes and expand the scope of mypy to utils.

--- src/ostorlab/utils/dictionary_minifier.py
+++ src/ostorlab/utils/dictionary_minifier.py
@@ -40,40 +40,9 @@ def minify_dict(value: Any, handler: Callable[[object], object]) -> Union[Dict[o
         return handler(value)
 
 
-def _nested_set(dic: Dict[object, object], keys: List[object], value: Any) -> None:
+def _nested_set(dic: Any, keys: List[object], value: Any) -> None:
     """Populates value in the correct place following the path."""
     for key in keys[:-1]:
         dic = dic.setdefault(key, {})
     dic[keys[-1]] = value
 
-
-def minify_dict_tail(dic: Dict, handler: Callable) -> Dict:
-    """Iterative approach to minify dictionary values.
-
-    Args:
-        dic: The dictionary to minify.
-        handler: Method that will be applyed to all the values.
-
-    Returns:
-        the minified version of the dict.
-    """
-    q = Queue()
-    for path, value in dic.items():
-        q.put(([path], value))
-
-    while True:
-        try:
-            path, value = q.get_nowait()
-            print('get', path, value)
-            if isinstance(value, list):
-                for i, nested_value in enumerate(value):
-                    q.put(([*path, i], nested_value))
-            elif isinstance(value, dict):
-                for key, nested_value in value.items():
-                    print('dict', [*path, key], nested_value)
-                    q.put(([*path, key], nested_value))
-            else:
-                value = handler(value)
-                _nested_set(dic, path, value)
-        except Empty:
-            return dic
