441da438d241ee036e1a4cb93a960e5c84071267
mohsinenar <mnarmouche@gmail.com>
2022-10-19 09:48:48 +0100

    Remove useless init method.

--- tests/assets/asset_test.py
+++ tests/assets/asset_test.py
@@ -9,11 +9,9 @@ def testAssetToProto_whenSelectorIsSetAndCorrect_generatesProto():
     @dataclasses.dataclass
     @asset.selector('v3.asset.file.android.apk')
     class SimpleAndroidApk(asset.Asset):
-        def __init__(self, content):
-            self.content: bytes = content
+        content: bytes
 
     raw = SimpleAndroidApk(b'test').to_proto()
-
     assert isinstance(raw, bytes)
     unraw = serializer.deserialize('v3.asset.file.android.apk', raw)
     assert unraw.content == b'test'
