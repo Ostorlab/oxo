1dadd0d92362d5c4454bbc9a669af1781aa56078
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-10-24 09:58:11 +0100

    Add vulnerability location to dumpers.

--- src/ostorlab/cli/dumpers.py
+++ src/ostorlab/cli/dumpers.py
@@ -5,7 +5,7 @@ import csv
 import json
 from typing import List
 
-FIELDNAMES = ['id', 'title', 'risk_rating', 'cvss_v3_vector', 'short_description', 'description', 'recommendation',
+FIELDNAMES = ['id', 'title', 'location', 'risk_rating', 'cvss_v3_vector', 'short_description', 'description', 'recommendation',
               'technical_detail']
 
 
