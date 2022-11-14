cf06b8f452261059653ba95af984ea9ee6138908
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-11-14 11:15:30 +0100

    Add content_url field to files assets.

--- src/ostorlab/assets/android_aab.py
+++ src/ostorlab/assets/android_aab.py
@@ -10,12 +10,19 @@ from ostorlab.assets import asset
 class AndroidAab(asset.Asset):
     """Android .AAB target asset."""
 
-    def __init__(self, content: bytes, path: Optional[str] = None):
+    def __init__(self, content: Optional[bytes] = None, path: Optional[str] = None, content_url: Optional[str] = None):
         self.content = content
         self.path = path
+        self.content_url = content_url
 
     def __str__(self) -> str:
-        return f'Android AAB({self.path})'
+        str_representation =  'Android AAB'
+        if self.path is not None:
+            str_representation =  f'{str_representation}:{self.path}'
+        if self.content_url is not None:
+            str_representation =  f'{str_representation}:{self.content_url}'
+
+        return str_representation
 
     @property
     def proto_field(self) -> str:
