8963a7d87766e9c7870ac364b6502d91ad0d99ac
amine <amine.mesbahi@ostorlab.dev>
2023-01-18 11:16:36 +0100

    Make fields optional.

--- src/ostorlab/agent/message/proto/v3/asset/file/source/source_pb2.py
+++ src/ostorlab/agent/message/proto/v3/asset/file/source/source_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n>ostorlab/agent/message/proto/v3/asset/file/source/source.proto\x12\x31ostorlab.agent.message.proto.v3.asset.file.source\"\'\n\x0f\x41ndroidMetadata\x12\x14\n\x0cpackage_name\x18\x01 \x02(\t\" \n\x0bIOSMetadata\x12\x11\n\tbundle_id\x18\x01 \x02(\t\"\x99\x02\n\x07Message\x12\x0f\n\x07\x63ontent\x18\x01 \x01(\x0c\x12\x0c\n\x04path\x18\x02 \x01(\t\x12\x13\n\x0b\x63ontent_url\x18\x03 \x01(\t\x12^\n\x10\x61ndroid_metadata\x18\x04 \x01(\x0b\x32\x42.ostorlab.agent.message.proto.v3.asset.file.source.AndroidMetadataH\x00\x12V\n\x0cios_metadata\x18\x05 \x01(\x0b\x32>.ostorlab.agent.message.proto.v3.asset.file.source.IOSMetadataH\x00\x12\x10\n\x08language\x18\x06 \x01(\tB\x10\n\x0emetadata_oneof')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n>ostorlab/agent/message/proto/v3/asset/file/source/source.proto\x12\x31ostorlab.agent.message.proto.v3.asset.file.source\"\'\n\x0f\x41ndroidMetadata\x12\x14\n\x0cpackage_name\x18\x01 \x01(\t\" \n\x0bIOSMetadata\x12\x11\n\tbundle_id\x18\x01 \x01(\t\"\x99\x02\n\x07Message\x12\x0f\n\x07\x63ontent\x18\x01 \x01(\x0c\x12\x0c\n\x04path\x18\x02 \x01(\t\x12\x13\n\x0b\x63ontent_url\x18\x03 \x01(\t\x12^\n\x10\x61ndroid_metadata\x18\x04 \x01(\x0b\x32\x42.ostorlab.agent.message.proto.v3.asset.file.source.AndroidMetadataH\x00\x12V\n\x0cios_metadata\x18\x05 \x01(\x0b\x32>.ostorlab.agent.message.proto.v3.asset.file.source.IOSMetadataH\x00\x12\x10\n\x08language\x18\x06 \x01(\tB\x10\n\x0emetadata_oneof')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'ostorlab.agent.message.proto.v3.asset.file.source.source_pb2', globals())
