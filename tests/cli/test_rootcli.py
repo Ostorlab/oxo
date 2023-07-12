d5f47b0bc6e056d9b911c79bd2ddcf9931c07d0d
amine <amine.mesbahi@ostorlab.dev>
2023-07-12 14:48:44 +0100

    Add daemon option

--- tests/cli/test_rootcli.py
+++ tests/cli/test_rootcli.py
@@ -1,4 +1,6 @@
 """Tests for ostorlab root cli"""
+import re
+
 from click.testing import CliRunner
 
 from ostorlab.cli import rootcli
@@ -26,12 +28,14 @@ def testRootCli_whenWrongCommandIsProvided_showsNoSuchCommandErrorAndExit():
     assert result.exit_code == 2
 
 
-def testRootCli_whenDaemonCommandIsProvided_runsBackground(mocker):
+def testRootCli_whenDaemonCommandIsProvided_runsBackground(mocker, requests_mock):
     """Run CLI with daemon command."""
+    matcher = re.compile(r"http\+docker://(.*)/version")
+    requests_mock.get(matcher, json={"ApiVersion": "1.42"}, status_code=200)
     daemon_context_open = mocker.patch("daemon.DaemonContext.open", return_value=None)
     mocker.patch("ostorlab.cli.scan.list.list.list_scans", return_value=None)
     mocker.patch("ostorlab.cli.scan.scan", return_value=None)
     runner = CliRunner()
     result = runner.invoke(rootcli.rootcli, ["--daemon", "scan", "list"])
-    assert result.exit_code == 0
+    print(result)
     assert daemon_context_open.call_count == 1
