326702330c3ef9e5187570f0b1030cfc8062b694
ErebusZ <azouiten@ostorlab.dev>
2023-07-12 14:13:30 +0100

    send scanner state

---
+++ src/ostorlab/apis/add_scanner_state.py
@@ -0,0 +1,61 @@
+
+from typing import Dict, Optional
+
+from ostorlab.apis import request
+from ostorlab.runtimes import reporter as rep
+
+
+class AddScannerStateAPIRequest(request.APIRequest):
+    """Report the scanner state."""
+
+    def __init__(self, state : rep.State):
+        """Sets the state values.
+
+        Args:
+           state: A dataclass instance holding state info of a runner.
+        """
+        self.state = state
+
+    @property
+    def query(self) -> Optional[str]:
+        """Defines the query to revoke the API key.
+
+        Returns:
+            The query to revoke the API key
+        """
+        return """
+        mutation AddScannerState($scannerState: ScannerStateInputType!) {
+          addScannerState(scannerState: $scannerState) {
+            scannerState {
+              scanId
+              scanner {
+                id
+              }
+              totalCpu
+              hostname
+            }
+          }
+        }
+    """
+
+    @property
+    def data(self) -> Optional[Dict]:
+        """Sets the variables to add the scanner state.
+
+        Returns:
+              The variables dict to add the scanner state.
+        """
+        data = {
+        "scannerState": {
+            "scannerId": self.state.scanner_id,
+            "scanId": self.state.scan_id,
+            "hostname": self.state.hostname,
+            "ipAddress": self.state.ip,
+            "memoryLoad": self.state.memory_load,
+            "totalMemory": self.state.total_memory,
+            "cpuLoad": self.state.cpu_load,
+            "totalCpu": self.state.total_cpu,
+            "errors": self.state.errors,
+        }
+    }
+        return data
---
+++ src/ostorlab/runtimes/reporter.py
@@ -0,0 +1,57 @@
+"""Reporter Doemon logic that report scanner state periodically."""
+import psutil
+import dataclasses
+import asyncio
+
+from ostorlab.apis.runners import public_runner
+from ostorlab.apis import add_scanner_state
+
+CAPTURE_INTERVAL = 300
+
+
+@dataclasses.dataclass
+class State:
+    """Current scanner state."""
+
+    scanner_id: int
+    scan_id: int
+    cpu_load: float
+    memory_load: float
+    total_cpu: int
+    total_memory: int
+    hostname: str
+    ip: str
+    errors: str
+
+class Reporter:
+    """Reporter collects information about the current scanner."""
+
+    def __init__(self, scanner_id: int, scan_id: int, hostname: str, ip: str, errors: str):
+        self._state = State(
+            scanner_id=scanner_id,
+            scan_id=scan_id,
+            cpu_load=0,
+            total_cpu=0,
+            memory_load=0,
+            total_memory=0,
+            hostname=hostname,
+            ip=ip,
+            errors=errors
+        )
+
+    def _capture_state(self):
+        """Capture current scanner state."""
+        self._state.cpu = psutil.cpu_percent(interval=1, percpu=False)
+        self._state.memory = psutil.virtual_memory().percent
+        self._state.total_cpu = psutil.cpu_count()
+        self._state.total_memory = psutil.virtual_memory().total
+
+    def _report_state(self):
+        runner = public_runner.PublicAPIRunner()
+        _ = runner.execute(add_scanner_state.AddScannerStateAPIRequest(state=self._state))
+
+    async def run(self):
+        while True:
+            await self._capture_state()
+            await self._report_state()
+            asyncio.sleep(CAPTURE_INTERVAL)
