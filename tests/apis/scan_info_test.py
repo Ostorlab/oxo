8c842221ad301ef380306b8832100ce75f21abba
falcon <o.essaji@ostorlab.dev>
2023-07-15 18:32:49 +0100

    Scan info api unitest

---
+++ tests/apis/scan_info_test.py
@@ -0,0 +1,24 @@
+import json
+from ostorlab.apis import scan_info
+
+
+def testScanInfoAPIRequest_always_returnsData() -> None:
+    scan_info_api = scan_info.ScanInfoAPIRequest(scan_id=12345)
+
+    assert scan_info_api._scan_id == 12345
+
+    expected_query = """
+         query scan($scanId: Int) {
+            scan(scanId: $scanId) {
+                progress
+                riskRating
+            }
+         }
+        """
+    assert scan_info_api.query == expected_query
+
+    expected_data = {
+        "query": expected_query,
+        "variables": json.dumps({"scanId": 12345}),
+    }
+    assert scan_info_api.data == expected_data
