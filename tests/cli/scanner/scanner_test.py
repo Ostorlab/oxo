3d6c7e76f77aed7984817fd8a21ab7c6fdb78a31
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2023-07-17 15:20:21 +0100

    Only run the daemon code in unix like systems.

--- tests/cli/scanner/scanner_test.py
+++ tests/cli/scanner/scanner_test.py
@@ -1,14 +1,18 @@
 """Unit tests for the ostorlab scanner subcommand."""
-from click.testing import CliRunner
+import sys
+
+import pytest
+from click import testing as click_testing
 
 from ostorlab.cli import rootcli
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 def testRootCli_whenDaemonCommandIsProvided_runsBackground(mocker, requests_mock):
     """Run CLI with daemon command."""
     daemon_context_open = mocker.patch("daemon.DaemonContext.open", return_value=None)
     mocker.patch("ostorlab.cli.scanner.scanner", return_value=None)
-    runner = CliRunner()
+    runner = click_testing.CliRunner()
     runner.invoke(
         rootcli.rootcli,
         ["--api-key", "test", "scanner", "--daemon", "--scanner-id", "11226DS"],
