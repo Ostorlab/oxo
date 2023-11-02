921daffdc4f4d2084aba6066f284c6469a8f6fd8
benyissa <benyissa@ostorlab.dev>
2023-11-02 10:03:28 +0100

    update fail_fast param to bool

--- src/ostorlab/agent/mixins/agent_mq_mixin.py
+++ src/ostorlab/agent/mixins/agent_mq_mixin.py
@@ -52,7 +52,7 @@ class AgentMQMixin:
         )
 
     async def _get_connection(self) -> aio_pika.abc.AbstractRobustConnection:
-        kwargs = {"fail_fast": "false"}
+        kwargs = {"fail_fast": False}
         return await aio_pika.connect_robust(url=self._url, loop=self._loop, **kwargs)
 
     async def _get_channel(self) -> aio_pika.Channel:
