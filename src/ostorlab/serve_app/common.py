7b34101f9110c7abf893d674478538db5cf6406a
mohsinenar <mnarmouche@gmail.com>
2024-05-29 11:46:28 +0100

    Appy ruff.

--- src/ostorlab/serve_app/common.py
+++ src/ostorlab/serve_app/common.py
@@ -12,6 +12,7 @@ import graphene
 
 class PageInfo(graphene.ObjectType):
     """Page info object type."""
+
     count = graphene.Int()
     num_pages = graphene.Int()
     has_next = graphene.Boolean()
