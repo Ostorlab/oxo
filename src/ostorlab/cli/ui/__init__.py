618e62e82f8965d04c7f58d2d8bf6a3762890633
mohsinenar <mnarmouche@gmail.com>
2024-06-11 18:08:59 +0100

    Add command to serve UI.

---
+++ src/ostorlab/cli/ui/__init__.py
@@ -0,0 +1,5 @@
+"""Module for the root command: scan."""
+
+from ostorlab.cli.ui import ui
+
+__all__ = ["ui"]
---
+++ tests/cli/ui/ui_test.py
@@ -0,0 +1,21 @@
+from click import testing
+from pytest_mock import plugin
+
+from ostorlab.cli import rootcli
+
+
+def testServeOxoUi_always_shouldCallHttpServeWithRightParams(
+        mocker: plugin.MockerFixture,
+
+) -> None:
+    """Ensure that the function call http.serve with the right params."""
+    mocked_http_serve = mocker.patch("http.server.HTTPServer.__init__", return_value=None)
+    mocked_serve_forever = mocker.patch("http.server.HTTPServer.serve_forever")
+    runner = testing.CliRunner()
+
+    result = runner.invoke(rootcli.rootcli, ["ui", "--port=9090", "--host=127.0.0.1"])
+
+    assert mocked_serve_forever.called is True
+    assert mocked_http_serve.called is True
+    assert mocked_http_serve.call_args[0][0] == ("127.0.0.1", 9090)
+    assert "Serving UI on : http://127.0.0.1:9090 ..." in result.output
