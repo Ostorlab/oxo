5db35047fe1957fd910326dedc8c4f9aeef6109a
elyousfi <mohamed.elyousfi@ostorlab.dev>
2024-08-12 16:33:07 +0100

    OXO should exit once the scan has completed

--- src/ostorlab/runtimes/local/log_streamer.py
+++ src/ostorlab/runtimes/local/log_streamer.py
@@ -1,13 +1,16 @@
 """Stream logs of a service from a thread."""
 
+import sys
 import threading
 from typing import Generator
 
 import docker.models.services
+from docker import errors as docker_errors
 
 from ostorlab.cli import console as cli_console
 
 console = cli_console.Console()
+docker_client = docker.from_env()
 
 COLOR_POOL = [
     "dodger_blue2",
@@ -40,6 +43,7 @@ class LogStream:
     def __init__(self):
         self._threads = []
         self._color_map = {}
+        self._active_services = []
 
     def stream(self, service: docker.models.services.Service) -> None:
         """Stream logs of a service without blocking.
@@ -51,11 +55,15 @@ class LogStream:
         """
         color = self._select_color(service)
         logs = service.logs(details=False, follow=True, stdout=True, stderr=True)
+        self._active_services.append(service.id)
         t = threading.Thread(
-            target=_stream_log, args=(logs, service.name, color), daemon=False
+            target=_stream_log, args=(logs, service.name, color), daemon=True
         )
         self._threads.append(t)
         t.start()
+
+        thread_stop_log = threading.Thread(target=self._check_services, daemon=False)
+        thread_stop_log.start()
 
     def _select_color(self, service):
         """Select color for console output of service."""
@@ -66,3 +74,15 @@ class LogStream:
             color = COLOR_POOL.pop()
             COLOR_POOL.insert(0, color)
         return color
+
+    def _check_services(self) -> None:
+        """Check if the services are still running."""
+        while True:
+            for service_id in list(self._active_services):
+                try:
+                    docker_client.services.get(service_id)
+                except docker_errors.NotFound:
+                    self._active_services.remove(service_id)
+            if len(self._active_services) == 0:
+                console.success("Scan completed.")
+                sys.exit(0)
