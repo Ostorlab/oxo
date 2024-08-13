af832d98d7b97d9c90d9d78abf3efbf275af917c
elyousfi <mohamed.elyousfi@ostorlab.dev>
2024-08-13 15:49:56 +0100

    refactor main and remote init.py

--- src/ostorlab/main.py
+++ src/ostorlab/main.py
@@ -1,5 +1,28 @@
-"""Run main function."""
+"""Ostorlab main package."""
 
-import ostorlab
+import logging
 
-ostorlab.main()
+import click
+from ostorlab.cli.rootcli import rootcli
+from rich import console
+from rich import logging as rich_logging
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="ERROR",
+    format=FORMAT,
+    datefmt="[%X]",
+    handlers=[
+        rich_logging.RichHandler(
+            rich_tracebacks=True,
+            console=console.Console(width=255),
+            tracebacks_suppress=[click],
+        )
+    ],
+)
+
+logger = logging.getLogger("CLI")
+
+
+def main():
+    rootcli(None)
