57e06f3f5d7fa76e7b7b122d1cc12e8b40637113
Mohamed Nasser <you@example.com>
2025-03-09 15:13:42 +0000

    Add from_dict asset factory methods

--- src/ostorlab/assets/asset.py
+++ src/ostorlab/assets/asset.py
@@ -11,6 +11,74 @@ class MissingTargetSelector(exceptions.OstorlabError):
     """Missing asset selector definition."""
 
 
+class UnknownSelector(exceptions.OstorlabError):
+    """Unknown selector value."""
+
+
+def from_dict_factory(slctr: str, data: dict[str, Any]) -> "Asset":
+    """Factory to create asset objects from a dictionary."""
+    if "v3.asset.file" == slctr:
+        from ostorlab.assets import file as file_asset
+
+        return file_asset.File.from_dict(data)
+    if "v3.asset.file.android.apk" == slctr:
+        from ostorlab.assets import android_apk
+
+        return android_apk.AndroidApk.from_dict(data)
+    if "v3.asset.file.android.aab" == slctr:
+        from ostorlab.assets import android_aab
+
+        return android_aab.AndroidAab.from_dict(data)
+    if "v3.asset.file.ios.ipa" == slctr:
+        from ostorlab.assets import ios_ipa
+
+        return ios_ipa.IOSIpa.from_dict(data)
+    if "v3.asset.file.ios.testflight" == slctr:
+        from ostorlab.assets import ios_testflight
+
+        return ios_testflight.IOSTestflight.from_dict(data)
+    if "v3.asset.file.api_schema" == slctr:
+        from ostorlab.assets import api_schema
+
+        return api_schema.ApiSchema.from_dict(data)
+    if "v3.asset.agent" in slctr:
+        from ostorlab.assets import agent as agent_asset
+
+        return agent_asset.Agent.from_dict(data)
+    if "v3.asset.store.android_store" == slctr:
+        from ostorlab.assets import android_store
+
+        return android_store.AndroidStore.from_dict(data)
+    if "v3.asset.store.ios_store" == slctr:
+        from ostorlab.assets import ios_store
+
+        return ios_store.IOSStore.from_dict(data)
+    if "v3.asset.domain_name" == slctr:
+        from ostorlab.assets import domain_name
+
+        return domain_name.DomainName.from_dict(data)
+    if "v3.asset.ip" == slctr:
+        from ostorlab.assets import ip as ip_asset
+
+        return ip_asset.IP.from_dict(data)
+    if "v3.asset.ip.v4" == slctr:
+        from ostorlab.assets import ipv4 as ipv4_asset
+
+        return ipv4_asset.IPv4.from_dict(data)
+    if "v3.asset.ip.v6" == slctr:
+        from ostorlab.assets import ipv6 as ipv6_asset
+
+        return ipv6_asset.IPv6.from_dict(data)
+    if "v3.asset.link" == slctr:
+        from ostorlab.assets import link as link_asset
+
+        return link_asset.Link.from_dict(data)
+
+    raise UnknownSelector(
+        f"Could not create asset object due to unknown selector: {slctr}"
+    )
+
+
 class Asset(abc.ABC):
     """Abstract Asset class to define the scan target and its properties."""
 
@@ -24,70 +92,6 @@ class Asset(abc.ABC):
     @property
     def proto_field(self) -> str:
         return "asset"
-
-
-def from_dict_factory(selector: str, data: dict[str, Any]) -> Asset:
-    """Factory to create asset objects from a dictionary."""
-    if "v3.asset.file" == selector:
-        from ostorlab.assets import file as file_asset
-
-        return file_asset.File.from_dict(data)
-    if "v3.asset.file.android.apk" == selector:
-        from ostorlab.assets import android_apk
-
-        return android_apk.AndroidApk.from_dict(data)
-    if "v3.asset.file.android.aab" == selector:
-        from ostorlab.assets import android_aab
-
-        return android_aab.AndroidAab.from_dict(data)
-    if "v3.asset.file.ios.ipa" == selector:
-        from ostorlab.assets import ios_ipa
-
-        return ios_ipa.IOSIpa.from_dict(data)
-    if "v3.asset.file.ios.testflight" == selector:
-        from ostorlab.assets import ios_testflight
-
-        return ios_testflight.IOSTestflight.from_dict(data)
-    if "v3.asset.file.api_schema" == selector:
-        from ostorlab.assets import api_schema
-
-        return api_schema.ApiSchema.from_dict(data)
-    if "v3.asset.agent" in selector:
-        from ostorlab.assets import agent as agent_asset
-
-        return agent_asset.Agent.from_dict(data)
-    if "v3.asset.store.android_store" == selector:
-        from ostorlab.assets import android_store
-
-        return android_store.AndroidStore.from_dict(data)
-    if "v3.asset.store.ios_store" == selector:
-        from ostorlab.assets import ios_store
-
-        return ios_store.IOSStore.from_dict(data)
-    if "v3.asset.domain_name" == selector:
-        from ostorlab.assets import domain_name
-
-        return domain_name.DomainName.from_dict(data)
-    if "v3.asset.ip" == selector:
-        from ostorlab.assets import ip as ip_asset
-
-        return ip_asset.IP.from_dict(data)
-    if "v3.asset.ip.v4" == selector:
-        from ostorlab.assets import ipv4 as ipv4_asset
-
-        return ipv4_asset.IPv4.from_dict(data)
-    if "v3.asset.ip.v6" == selector:
-        from ostorlab.assets import ipv6 as ipv6_asset
-
-        return ipv6_asset.IPv6.from_dict(data)
-    if "v3.asset.link" == selector:
-        from ostorlab.assets import link as link_asset
-
-        return link_asset.Link.from_dict(data)
-
-    raise ValueError(
-        f"Could not create asset object due to unknown selector: {selector}"
-    )
 
 
 def selector(target: str) -> Callable[[Type[Asset]], Type[Asset]]:
