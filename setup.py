94d8b1f115476fbd615e4737641a0d92e44bc659
ostorlab <support@ostorlab.co>
2025-10-03 16:52:02 +0100

    Rebuild all protobuf files
    - Rebuilt 74 protobuf files (73 v3 + 1 common)
    - Used grpcio-tools protoc compiler v31.1
    - Maintained proper package structure from src/ directory
    - Updated all _pb2.py files with latest protobuf definitions

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
             "please ensure you have the most updated version of setuptools, "
             "setuptools_scm and wheel with:\n"
             "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
+)
         raise
