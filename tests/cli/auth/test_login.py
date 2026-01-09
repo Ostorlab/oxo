b3b1262cf60b6553dad6f074d7c9c375a9774cc2
Oussama-Bakri <oussama.bakri@ostorlab.dev>
2026-01-09 16:11:38 +0100

    fix test

--- tests/cli/auth/test_login.py
+++ tests/cli/auth/test_login.py
@@ -37,32 +37,13 @@ def testOstorlabAuthLoginCLI_whenValidLoginCredentialsAreProvided_tokenSet(
     httpx_mock,
 ):
     """Test ostorlab auth login command with valid login credentials (no otp required).
-    Should set API key.
+    Should set authorization token.
     """
 
-    api_key_dict = {
-        "data": {
-            "createApiKey": {
-                "apiKey": {
-                    "secretKey": "ADABYMTu.S7Y8zmKxpbgTcSuGmsC3rkPdAs95yMwW",
-                    "apiKey": {
-                        "expiryDate": None,
-                        "id": "54a6e602-f7c1-47bb-9ca0-af598fcf3cf4",
-                    },
-                }
-            }
-        }
-    }
     token_dict = {"token": "2fd7a589-64b4-442e-95aa-eb0d082aab63"}
     runner = CliRunner()
     httpx_mock.add_response(
         method="POST", url=login_runner.TOKEN_ENDPOINT, json=token_dict, status_code=200
-    )
-    httpx_mock.add_response(
-        method="POST",
-        url=authenticated_runner.AUTHENTICATED_GRAPHQL_ENDPOINT,
-        json=api_key_dict,
-        status_code=200,
     )
     result = runner.invoke(
         rootcli.rootcli, ["auth", "login"], input="username\npassword"
@@ -70,8 +51,8 @@ def testOstorlabAuthLoginCLI_whenValidLoginCredentialsAreProvided_tokenSet(
 
     assert result.exception is None
     assert (
-        configuration_manager.ConfigurationManager().api_key
-        == api_key_dict["data"]["createApiKey"]["apiKey"]["secretKey"]
+        configuration_manager.ConfigurationManager().authorization_token
+        == token_dict["token"]
     )
 
 
