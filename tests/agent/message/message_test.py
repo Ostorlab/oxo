ec5069d0df1031d5e2d2b37ab591cfe86ca790c2
burogurama <mohamed.nasser@ostorlab.dev>
2026-02-20 13:57:17 +0000

    Add domain_name to v3.fingerprint.domain_name#

--- tests/agent/message/message_test.py
+++ tests/agent/message/message_test.py
@@ -66,3 +66,33 @@ def testMessageSerializeDeserialize_Forv3CaptureFilesystem_generatesProperProto(
     assert isinstance(serialized.raw, bytes)
     assert deserialized.data.get("event") == "ACCESS"
     assert deserialized.data.get("filename") == "/etc/hosts"
+
+
+def testMessageSerializeDeserialize_Forv3FingerprintDomainNameServiceLibrary_generatesProperProto():
+    """Test message proper serialization from a dict object to a protobuf based on the selector."""
+    serialized = message.Message.from_data(
+        "v3.fingerprint.domain_name.service.library",
+        {
+            "name": "test_service",
+            "port": 8080,
+            "schema": "https",
+            "library_name": "test_library",
+            "library_version": "1.0.0",
+            "library_type": "test_type",
+            "detail": "test_detail",
+            "domain_name": "example.com",
+        },
+    )
+    deserialized = message.Message.from_raw(
+        "v3.fingerprint.domain_name.service.library", serialized.raw
+    )
+
+    assert isinstance(serialized.raw, bytes)
+    assert deserialized.data.get("name") == "test_service"
+    assert deserialized.data.get("port") == 8080
+    assert deserialized.data.get("schema") == "https"
+    assert deserialized.data.get("library_name") == "test_library"
+    assert deserialized.data.get("library_version") == "1.0.0"
+    assert deserialized.data.get("library_type") == "test_type"
+    assert deserialized.data.get("detail") == "test_detail"
+    assert deserialized.data.get("domain_name") == "example.com"
