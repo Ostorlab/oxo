cd7248da7a7044dee890a67dc3c0c974dfb5abeb
ohachimOs <ohachim@ostorlab.dev>
2026-03-18 11:22:16 +0000

    Change to queue_name

--- src/ostorlab/runtimes/local/agent_runtime.py
+++ src/ostorlab/runtimes/local/agent_runtime.py
@@ -389,6 +389,7 @@ class AgentRuntime:
             healthcheck=self.create_docker_healthchek(),
             labels={
                 "ostorlab.universe": self.runtime_name,
+                ## queue_name is used by the autoscaler to identify to check agent queues and to decide on exclusion/inclusion.
                 "ostorlab.queue_name": explicit_service_name
                 if explicit_service_name is not None
                 else agent_definition.name,
