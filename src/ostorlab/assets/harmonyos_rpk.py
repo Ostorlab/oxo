75bd22394ac7183d071cde30012f35c481d86d5b
yassminassaii <yassmine.attar@ostorlab.dev>
2026-03-25 17:53:30 +0100

    fix ruff

--- src/ostorlab/assets/harmonyos_rpk.py
+++ src/ostorlab/assets/harmonyos_rpk.py
@@ -9,21 +9,21 @@ from ostorlab.assets import asset
 @dataclasses.dataclass
 @asset.selector("v3.asset.file.harmonyos.rpk")
 class HarmonyOSRpk(asset.Asset):
-	"""HarmonyOS RPK target asset."""
+    """HarmonyOS RPK target asset."""
 
-	content: Optional[bytes] = None
-	path: Optional[str] = None
-	content_url: Optional[str] = None
+    content: Optional[bytes] = None
+    path: Optional[str] = None
+    content_url: Optional[str] = None
 
-	def __str__(self) -> str:
-		str_representation = "HarmonyOS RPK"
-		if self.path is not None:
-			str_representation = f"{str_representation}:{self.path}"
-		if self.content_url is not None:
-			str_representation = f"{str_representation}:{self.content_url}"
+    def __str__(self) -> str:
+        str_representation = "HarmonyOS RPK"
+        if self.path is not None:
+            str_representation = f"{str_representation}:{self.path}"
+        if self.content_url is not None:
+            str_representation = f"{str_representation}:{self.content_url}"
 
-		return str_representation
+        return str_representation
 
-	@property
-	def proto_field(self) -> str:
-		return "harmonyos_rpk"
+    @property
+    def proto_field(self) -> str:
+        return "harmonyos_rpk"
