77807b5fc10c2e350f674e6eeaf59ff88cf6e073
amat-osto <support@ostorlab.co>
2026-05-12 16:57:40 +0100

    fix: avoid @None in Repository.__str__ when commit_hash is unset

--- src/ostorlab/assets/repository.py
+++ src/ostorlab/assets/repository.py
@@ -18,7 +18,9 @@ class Repository(asset.Asset):
 
     def __str__(self) -> str:
         if self.repo_url is not None:
-            return f"Repository: {self.repo_url}@{self.commit_hash}"
+            if self.commit_hash is not None:
+                return f"Repository: {self.repo_url}@{self.commit_hash}"
+            return f"Repository: {self.repo_url}"
         if self.content_url is not None:
             return f"Repository: {self.content_url}"
         return "Repository"
