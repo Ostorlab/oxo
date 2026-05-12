a7b9bbf1bb7d37aba52640c62dbec846cc99c786
amat-osto <support@ostorlab.co>
2026-05-12 14:31:12 +0100

    feature: add repository asset proto for source code scanning
    Adds v3/asset/repository/repository.proto as a first-class asset for
    SCM-backed repository scans. Updates VulnerabilityLocation to support
    repository as a valid location target.

---
+++ tests/agent/message/proto/v3/asset/repository/repository_pb2_test.py
@@ -0,0 +1,57 @@
+from ostorlab.agent.message.proto.v3.asset.repository import repository_pb2
+
+
+def testMessage_whenCreateWithValidData_shouldSerializeAndDeserializeCorrectly():
+    message = repository_pb2.Message()
+    message.content_url = "https://storage.example.com/repo.zip"
+    message.repo_url = "https://github.com/org/repo.git"
+    message.commit_hash = "a1a10cdbc6551ba359169a3033f193b7f8c1b95d"
+    message.scm_provider = repository_pb2.GITHUB
+
+    serialized = message.SerializeToString()
+    deserialized = repository_pb2.Message()
+    deserialized.ParseFromString(serialized)
+
+    assert deserialized.content_url == "https://storage.example.com/repo.zip"
+    assert deserialized.repo_url == "https://github.com/org/repo.git"
+    assert deserialized.commit_hash == "a1a10cdbc6551ba359169a3033f193b7f8c1b95d"
+    assert deserialized.scm_provider == repository_pb2.GITHUB
+
+
+def testMessage_whenCreateWithContentBytes_shouldSerializeAndDeserializeCorrectly():
+    message = repository_pb2.Message()
+    message.content = b"PK\x03\x04archive_content"
+    message.repo_url = "https://gitlab.com/org/repo.git"
+    message.commit_hash = "b2b20cdbc6551ba359169a3033f193b7f8c1b95e"
+    message.scm_provider = repository_pb2.GITLAB
+
+    serialized = message.SerializeToString()
+    deserialized = repository_pb2.Message()
+    deserialized.ParseFromString(serialized)
+
+    assert deserialized.content == b"PK\x03\x04archive_content"
+    assert deserialized.scm_provider == repository_pb2.GITLAB
+
+
+def testMessage_whenCreateEmpty_shouldHaveDefaultValues():
+    message = repository_pb2.Message()
+
+    assert message.content == b""
+    assert message.content_url == ""
+    assert message.repo_url == ""
+    assert message.commit_hash == ""
+    assert message.scm_provider == repository_pb2.UNKNOWN
+
+
+def testMessage_whenSerializeEmpty_shouldDeserializeToEmpty():
+    message = repository_pb2.Message()
+
+    serialized = message.SerializeToString()
+    deserialized = repository_pb2.Message()
+    deserialized.ParseFromString(serialized)
+
+    assert deserialized.content == b""
+    assert deserialized.content_url == ""
+    assert deserialized.repo_url == ""
+    assert deserialized.commit_hash == ""
+    assert deserialized.scm_provider == repository_pb2.UNKNOWN
