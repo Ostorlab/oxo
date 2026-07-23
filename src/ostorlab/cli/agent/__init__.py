7709b2e5c414921e8bdb57a4f61cfacaa0468a2f
aitougrram <mouad.aitougrram@ostorlab.dev>
2026-07-23 21:05:10 +0100

    fix issues

--- src/ostorlab/cli/agent/__init__.py
+++ src/ostorlab/cli/agent/__init__.py
@@ -1,9 +1,9 @@
 """Module for the root command: agent."""
 
+from ostorlab.cli.agent.agent import agent
 from ostorlab.cli.agent import build, delete, healthcheck, install
 from ostorlab.cli.agent import list as list_cli
 from ostorlab.cli.agent import search as search_cli
-from ostorlab.cli.agent.agent import agent
 
 __all__ = [
     "agent",
--- src/ostorlab/cli/ci_scan/__init__.py
+++ src/ostorlab/cli/ci_scan/__init__.py
@@ -1,6 +1,6 @@
 """Module for the root command ci_scan"""
 
-from ostorlab.cli.ci_scan import run
 from ostorlab.cli.ci_scan.ci_scan import ci_scan
+from ostorlab.cli.ci_scan import run
 
 __all__ = ["ci_scan", "run"]
